1000b5cb04c9848f31d960a7da175e6114b7bd7f
robisonwebdev <david@robisonweb.dev>
2021-10-26 11:09:03 -0600

    Imported NavLink component and added to return.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NavLink from './NavLink';
 
 const Nav = () => {
     return (
         <nav>
-            
+            <NavLink />
         </nav>
     )
 }
