685a76b4dc5a8fd8e9aa5c6b5ce4b8b7287cd42a
robisonwebdev <david@robisonweb.dev>
2021-10-26 10:50:40 -0600

    Destrucuted address, name, and text. Added small tag inside footer, and 'a' tag inside small. Passed props down into return.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/Footer.css';
 
-const Footer = () => {
+const Footer = ({ address, name, text }) => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <small>{text}<a href={address} target='_blank' rel='noreferrer'>{name}</a></small>
+    </footer>
   );
 }
 
