48019d0bb03e5c233646b0fcb8b150fcd5c535cd
robisonwebdev <david@robisonweb.dev>
2021-10-26 10:58:52 -0600

    Imported Logo and Nav components. Added both components to return header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Logo from './Logo';
+import Nav from './Nav';
 import '../styles/Header.css';
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header>
+      <Logo />
+      <Nav />
+    </header>
   );
 }
 
