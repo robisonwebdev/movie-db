017e15e0ad27d9a3d9cd3f4f731da7e56c906946
robisonwebdev <david@robisonweb.dev>
2021-10-27 10:30:55 -0600

    Imported useEffect and useState. Setup useState for apiKey, loading, and popularMovies. Setup a useEffect on page load to get the first page on popular movies from api. Set reaults to popularMovies state. Setup temp useEffect to console.log popularMovies.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../styles/Main.css';
 
 const Main = () => {
+  const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+  const [loading, setLoading] = useState(true);
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  // Get popular movies on load
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+      .then(res => {
+        setPopularMovies(res.data.results)
+        setLoading(false);
+      })
+      .catch(err => console.log(err))
+    
+  }, []);
+
+  // Temp useEffect
+  useEffect(() => {
+    if (!loading) {
+      console.log(popularMovies);
+    }
+  }, [loading]);
+
   return (
     <main>Main</main>
   );
