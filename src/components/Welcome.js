fd77fc630b7c7278b43e9efc95c3ab1c4eab8198
robisonwebdev <david@robisonweb.dev>
2021-10-28 10:28:07 -0600

    Imported Search component. Moved h1 and h2 into a new div. Added Search component below new div."

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Search from './Search';
 import '../styles/Welcome.css';
 
 const Welcome = () => {
     return (
         <div className='welcome'>
-            <h1>Welcome.</h1>
-            <h2>Millions of movies, TV shows and people to discover. Explore now.</h2>
+            <div>
+                <h1>Welcome.</h1>
+                <h2>Millions of movies, TV shows and people to discover. Explore now.</h2>
+            </div>
+            <Search />
         </div>
     )
 };
