9ae12dda1b8f15cb0fdcf8e0c99b78488595f1fe
robisonwebdev <david@robisonweb.dev>
2021-10-29 11:57:27 -0600

    Destructured movie. Created variables to get poster link, movie title, and release date. Added img tag to return div. Set src to poster and alt to title. Added h3 and set inner text to title. Imported and added ShowDate. Set value to releaseDate.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ShowDate from './ShowDate';
 
-const Card = () => {
+const Card = ({ movie }) => {
+    const poster = `https://image.tmdb.org/t/p/w185/${movie.poster_path}`;
+    const title = movie.title;
+    const releaseDate = movie.release_date;
+
     return (
-        <div>
-            card
+        <div className='card'>
+            <img src={poster} alt={`${title} poster`} />
+            <h3>{title}</h3>
+            <ShowDate value={releaseDate} />
         </div>
     )
 };
