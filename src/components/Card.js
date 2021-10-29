ff2e888212e39d9123259188b67ea09d622f55e4
robisonwebdev <david@robisonweb.dev>
2021-10-29 12:02:52 -0600

    Changed h3 to h4

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ const Card = ({ movie }) => {
     return (
         <div className='card'>
             <img src={poster} alt={`${title} poster`} />
-            <h3>{title}</h3>
+            <h4>{title}</h4>
             <ShowDate value={releaseDate} />
         </div>
     )
