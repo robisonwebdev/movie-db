145234e0fca365f5b5a7e17c381f3f2c21a13ce4
robisonwebdev <david@robisonweb.dev>
2021-10-30 04:06:40 -0600

    Imported Sort component. Gave return div a className of filterPanel. Added Sort component to return div.

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sort from './Sort';
 
 const FilterPanel = () => {
     return (
-        <div>
-            
+        <div className='filterPanel'>
+            <Sort />
         </div>
     )
 };
