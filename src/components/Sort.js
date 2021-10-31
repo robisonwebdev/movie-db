2d5dc991e57d5197278da3ecfd213475d92bb14f
robisonwebdev <david@robisonweb.dev>
2021-10-31 00:28:23 -0600

    Moved handleSubmit and .sortContent div to SortForm.js Setup ternery to add SortForm if showContent is true.

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react';
+import SortForm from './SortForm';
 import '../styles/Sort.css';
 
 const Sort = () => {
     const [showContent, setShowContent] = useState(true);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
-
     return (
         <div className='sort'>
             <div className='sortTitle'>Sort</div>
-            <div className={`sortContent ${showContent ? 'showContent' : ''}`}>
-                <form className='sortForm' onSubmit={handleSubmit}>
-                    <label>Sort Results By</label>
-                    <select>
-                        <option>Popularity Descending</option>
-                        <option>Popularity Ascending</option>
-                        <option>Rating Descending</option>
-                        <option>Rating Ascending</option>
-                        <option>Release Date Descending</option>
-                        <option>Release Date Ascending</option>
-                        <option>Title (A-Z)</option>
-                        <option>Title (Z-A)</option>
-                    </select>
-                </form>
-            </div>
+            {showContent ? <SortForm /> : null}
         </div>
     )
 };
