a15cf3916aba345faf535b18b2e43e95efd77020
robisonwebdev <david@robisonweb.dev>
2021-10-31 01:01:17 -0600

    Created function called handleSortClick, which calls setShowContent and passes opposite of showContent. Passed handleSortClick to onClick on sortTitle div. Inside sortTitle div, put Sort text into p tag and setup ternery for arrow icons.

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,11 +3,18 @@ import SortForm from './SortForm';
 import '../styles/Sort.css';
 
 const Sort = () => {
-    const [showContent, setShowContent] = useState(true);
+    const [showContent, setShowContent] = useState(false);
+
+    const handleSortClick = () => {
+        setShowContent(!showContent);
+    };
 
     return (
         <div className='sort'>
-            <div className='sortTitle'>Sort</div>
+            <div className='sortTitle' onClick={handleSortClick}>
+                <p>Sort</p>
+                {showContent ? <i className='las la-angle-down' /> : <i className='las la-angle-right' />}
+            </div>
             {showContent ? <SortForm /> : null}
         </div>
     )
