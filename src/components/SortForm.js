8d93fe34c551488ebde0e039b081d1e4d79aba16
robisonwebdev <david@robisonweb.dev>
2021-10-31 00:25:30 -0600

    Removed .sortContent div from Sort.js and moved it into its own component.

--- src/components/SortForm.js
+++ src/components/SortForm.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const SortForm = () => {
     return (
-        <div>
-            
+        <div className='sortContent'>
+            <form className='sortForm' onSubmit={handleSubmit}>
+                <label>Sort Results By</label>
+                <select>
+                    <option>Popularity Descending</option>
+                    <option>Popularity Ascending</option>
+                    <option>Rating Descending</option>
+                    <option>Rating Ascending</option>
+                    <option>Release Date Descending</option>
+                    <option>Release Date Ascending</option>
+                    <option>Title (A-Z)</option>
+                    <option>Title (Z-A)</option>
+                </select>
+            </form>
         </div>
     )
 };
