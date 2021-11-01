6aaffd99a3e5543ad92ce692582d0231a1572849
robisonwebdev <david@robisonweb.dev>
2021-11-01 15:18:04 -0600

    Imported useState. Setup useState for openCard, with an initial value of false.

--- src/components/FilterCard.js
+++ src/components/FilterCard.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/FilterCard.css';
 
 const FilterCard = ({ component, title }) => {
+    const [openCard, setOpenCard] = useState(false);
+
     return (
         <div className='filterCard'>
             <p className='filterCardTitle'>{title}</p>
