4c8730b9932a7954da0cfbd0cdba93f40cdfbfc5
robisonwebdev <david@robisonweb.dev>
2021-11-02 09:46:42 -0600

    Imported useState. Setup sort useState, with an initial value of 'pop_des'. Passes sort state to FilterCard component under value prop.

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilterCard from './FilterCard';
 import Sort from './Sort';
 
 const FilterPanel = () => {
+    const [sort, setSort] = useState('pop_des')
+
     return (
         <div className='filterPanel'>
             <FilterCard
-                title='Sort'
                 component={<Sort />}
+                title='Sort'
+                value={sort}
             />
         </div>
     )
