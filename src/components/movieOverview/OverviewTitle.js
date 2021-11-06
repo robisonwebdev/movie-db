7afe5bee9178551e29fb350fbc46476deb22114a
robisonwebdev <david@robisonweb.dev>
2021-11-06 11:45:15 -0600

    Destructured props. Gave return div a className of overviewTitle. Added h1 and div with className of overviewFacts to return div. Set h1 to title prop. Inside overviewFacts, added remaining props to individual 'p' elements.

--- src/components/movieOverview/OverviewTitle.js
+++ src/components/movieOverview/OverviewTitle.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const OverviewTitle = () => {
+const OverviewTitle = ({ certification, genres, releaseDate, runtime, title }) => {
     return (
-        <div>
-            Overview Title
+        <div className='overviewTitle'>
+            <h1>{title}</h1>
+            <div className='overviewFacts'>
+                <p>{certification}</p>
+                <p>{releaseDate}</p>
+                <p>{genres}</p>
+                <p>{runtime}</p>
+            </div>
         </div>
     )
 };
