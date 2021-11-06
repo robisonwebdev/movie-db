4d102d01b4a671db6d72f07caf37895cd47ec900
robisonwebdev <david@robisonweb.dev>
2021-11-06 11:32:15 -0600

    Setup initial functional OverviewDescription component.

---
+++ src/components/movieOverview/OverviewDescription.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OverviewDescription = () => {
+    return (
+        <div>
+            Overview Description
+        </div>
+    )
+};
+
+export default OverviewDescription;
