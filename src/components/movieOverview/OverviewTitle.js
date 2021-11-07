594b790c033385c9cec0312875173547c82fe2a5
robisonwebdev <david@robisonweb.dev>
2021-11-07 12:21:54 -0700

    Destrucuted credits, movie, and releaseDate. Removed old destrcutred props. Setup variables to get title, certification, date, genres, and runtime.

--- src/components/movieOverview/OverviewTitle.js
+++ src/components/movieOverview/OverviewTitle.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const OverviewTitle = ({ certification, genres, releaseDate, runtime, title }) => {
+const OverviewTitle = ({ credits, movie, releaseDate }) => {
+    const title = movie.title;
+    const certification = releaseDate['release_dates'][0]['certification'];
+    const date = new Date(releaseDate['release_dates'][0]['release_date']).getUTCFullYear();
+    const genres = movie.genres[0]['name'];
+    const runtime = movie.runtime;
+
     return (
         <div className='overviewTitle'>
             <h1>{title}</h1>
             <div className='overviewFacts'>
                 <p>{certification}</p>
-                <p>{releaseDate}</p>
+                <p>{date}</p>
                 <p>{genres}</p>
                 <p>{runtime}</p>
             </div>
