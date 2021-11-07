f603e15f7d5c92bc0174f1895d746dd431241b81
robisonwebdev <david@robisonweb.dev>
2021-11-07 11:36:58 -0700

    Imported useState, useEffect, and Axios. Setup useState for apiKey, creditData, loading, movieData, and releaseData. Setup useEffect to get api information for movieData, ReleaseDateData, and CreditData. Setup temp useEffect to console.log all useState datas.

--- src/components/movieOverview/Overview.js
+++ src/components/movieOverview/Overview.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import OverviewHeader from './OverviewHeader';
 
-const Overview = () => {
+const Overview = ({ movieID }) => {
+    const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [creditData, setCreditData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [movieData, setMovieData] = useState([]);
+    const [releaseDateData, setReleaseDateData] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+
+        axios
+            .get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&append_to_response=release_dates,credits&&language=en-US`)
+            .then(res => {
+                setMovieData(res.data);
+                setReleaseDateData(res.data['release_dates']['results'][20]);
+                setCreditData(res.data.credits);
+                setLoading(false);
+            })
+            .catch(err => console.log(err))
+    }, [apiKey]);
+
+    useEffect(() => {
+        if (!loading) {
+          console.log('MovieData:', movieData);
+          console.log('Release Date:', releaseDateData);
+          console.log('Credits:', creditData);
+        }
+      }, [loading, movieData, releaseDateData, creditData]);
+
     return (
         <div>
             <OverviewHeader />
