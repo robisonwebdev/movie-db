0da5e55b3782e587c5328c7b8086ea180b38b651
robisonwebdev <david@robisonweb.dev>
2021-11-07 10:23:41 -0700

    Imported Logo component. Added Logo component to return nav.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavLink from './NavLink';
+import Logo from './Logo';
 import '../styles/Navbar.css';
 
 const Nav = () => {
     return (
         <nav>
+            <Logo />
             <ul>
                 <NavLink linkName='Movies' />
                 <NavLink linkName='TV Shows' />
