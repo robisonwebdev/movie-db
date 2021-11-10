2913f83ba608cbf939e0e85fc084354893d2f4d3
robisonwebdev <david@robisonweb.dev>
2021-11-10 09:18:22 -0700

    Imported useState and useEffect. Destructured credits. Setup useState for crew, director, producer, and screenplay. Setup useEffect on crew change to find and set state on director, producer, and screenplay. Setup Crew component and accept job and name prop. Called Crew component in OverviewCrew component.

--- src/components/movieOverview/OverviewCrew.js
+++ src/components/movieOverview/OverviewCrew.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const OverviewCrew = () => {
+const OverviewCrew = ({ credits }) => {
+    const [crew] = useState(credits.crew);
+    const [director, setDirector] = useState('');
+    const [producer, setProducer] = useState('');
+    const [screenplay, setScreenplay] = useState('');
+
+    useEffect(() => {
+        // Get Director
+        crew.find(member => {
+            if (member.job === 'Director') {
+                setDirector(member.name);
+            }
+
+            return null;
+        })
+
+        // Get Screenplay
+        crew.find(member => {
+            if (member.job === 'Screenplay') {
+                setScreenplay(member.name);
+            }
+
+            return null;
+        })
+
+        // Get Producer
+        crew.find(member => {
+            if (member.job === 'Producer') {
+                setProducer(member.name);
+            }
+
+            return null;
+        })
+    }, [crew]);
+
+    console.log('Crew:', crew);
     return (
-        <div>
-            Overview Crew
+        <div className='overviewCrew'>
+            <Crew job={'Director'} name={director} />            
+            <Crew job={'Producer'} name={producer} />            
+            <Crew job={'Screenplay'} name={screenplay} />            
         </div>
     )
 };
 
 export default OverviewCrew;
+
+const Crew = ({ job, name }) => {
+    return (
+        <div className='crew'>
+            <h4>{name}</h4>
+            <p>{job}</p>
+        </div>
+    );
+}
