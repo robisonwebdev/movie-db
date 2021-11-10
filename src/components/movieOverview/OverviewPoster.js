62bfb77fb289170ba810a81877f11794f7987c4c
robisonwebdev <david@robisonweb.dev>
2021-11-10 07:47:43 -0700

    Gave img a className of overviewPoster

--- src/components/movieOverview/OverviewPoster.js
+++ src/components/movieOverview/OverviewPoster.js
@@ -5,7 +5,7 @@ const OverviewPoster = ({ movie }) => {
     const poster = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`;
 
     return (
-        <img src={poster} alt={movie.title} />
+        <img className='overviewPoster' src={poster} alt={movie.title} />
     )
 };
 
