fb8aa95d1feb9216a4439da07b39c1afd83ce13e
robisonwebdev <david@robisonweb.dev>
2021-11-14 12:56:27 -0700

    Setup getLanguageString to match original_language to languagues.english_name and return. Removed original_language from .originalLanguage and replaced with getLanguageString.

--- src/components/movieOverview/MovieStats.js
+++ src/components/movieOverview/MovieStats.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
-const MovieStats = ({ stats }) => {
+const MovieStats = ({ languages, stats }) => {
     const { budget, original_language, revenue, status } = stats;
+
+    const getLanguageString = () => {  
+        let language = '';
+
+        languages.forEach(obj => {
+            if (obj['iso_639_1'] === original_language) {
+                language = obj.english_name;
+            }
+        })
+
+        return language;
+    }
 
     return (
         <div className='movieStats'>
-            {console.log(stats)}
+            {}
             <div className='status'>
                 <h4>Status</h4>
                 {status}
             </div>
             <div className='originalLanguage'>
                 <h4>Original Language</h4>
-                {original_language}
+                {getLanguageString()}
             </div>
             <div className='budget'>
                 <h4>Budget</h4>
