91e9ae0b91281323b0700cec757e29d3b21db888
robisonwebdev <david@robisonweb.dev>
2021-11-14 10:49:31 -0700

    Destructured stats. Destrucutured multiple objects inside component. Gave return div a className of movieStats. Inside return div setup multiple divs with classNames of .status, .originalLanguage, budget, and revenue. Inside each non-return div, added h4 and prop.

--- src/components/movieOverview/MovieStats.js
+++ src/components/movieOverview/MovieStats.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const MovieStats = () => {
+const MovieStats = ({ stats }) => {
+    const { budget, original_language, revenue, status } = stats;
+
     return (
-        <div>
-            Stats
+        <div className='movieStats'>
+            {console.log(stats)}
+            <div className='status'>
+                <h4>Status</h4>
+                {status}
+            </div>
+            <div className='originalLanguage'>
+                <h4>Original Language</h4>
+                {original_language}
+            </div>
+            <div className='budget'>
+                <h4>Budget</h4>
+                {budget}
+            </div>
+            <div className='revenue'>
+                <h4>Revenue</h4>
+                {revenue}
+            </div>
         </div>
     )
 };
