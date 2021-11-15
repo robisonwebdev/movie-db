0943fefb8066a25a6961602e773031b60ee18e6c
robisonwebdev <david@robisonweb.dev>
2021-11-15 09:40:23 -0700

    Imported Welcome.js Gave return div a className of home. Added Welcome component to home div.

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Welcome from './Welcome';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='home'>
+            <Welcome />
         </div>
     )
 };
