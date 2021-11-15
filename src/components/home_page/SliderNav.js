f59d9cd4d1eac6a6ada21ce8d86c56df3e1d9f53
robisonwebdev <david@robisonweb.dev>
2021-11-15 11:32:39 -0700

    Setup initial functional SliderNav component.

---
+++ src/components/home_page/SliderNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SliderNav = () => {
+    return (
+        <nav className='sliderNav'>
+            Slider Nav
+        </nav>
+    )
+};
+
+export default SliderNav;
