a1383324f52f8ad13a24f8aebcc02aa1ce55702b
robisonwebdev <david@robisonweb.dev>
2021-11-15 11:50:42 -0700

    Destructured selectors and passed it to SliderNav

--- src/components/home_page/Discover.js
+++ src/components/home_page/Discover.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import SliderNav from './SliderNav';
 
-const Discover = ({ className, title }) => {
+const Discover = ({ className, selectors, title }) => {
     return (
         <section className='discover'>
             <div className={className}>
                 <div className={'discover_header'}>
                     <h2>{title}</h2>
-                    <SliderNav />
+                    <SliderNav selectors={selectors}  />
                 </div>
                 <div className={'discover_content'}>
                     {/* Cards */}
