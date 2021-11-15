85b22ad59055523187ff15dad50e0d65a0af92e8
robisonwebdev <david@robisonweb.dev>
2021-11-15 11:26:23 -0700

    Imported Discover.js and added it below Welcome component. Passed className and title prop to Discover component.

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Welcome from './Welcome';
+import Discover from './Discover';
 
 const Home = () => {
     return (
         <div className='home'>
             <Welcome />
+            <Discover className='whatsPopular' title="What's Popular" />
         </div>
     )
 };
