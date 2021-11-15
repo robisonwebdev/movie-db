71c964cfeb29376a950f79da848ceda8dc806c31
robisonwebdev <david@robisonweb.dev>
2021-11-15 09:42:15 -0700

    Removed temp imports. Imported Home.js Removed temp Components from return and replaced with Home component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Welcome from './Welcome';
+import Home from './home_page/Home';
 import '../styles/Main.css';
 
 const Main = () => {
   const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
   const [loading, setLoading] = useState(true);
   const [popularMovies, setPopularMovies] = useState([]);
+  const [movieID, setMovieID] = useState('580489');
 
   // Get popular movies on load
   useEffect(() => {
     setLoading(true);
 
     axios
-      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&sort_bypage=1`)
       .then(res => {
         setPopularMovies(res.data.results)
         setLoading(false);
@@ -31,7 +32,7 @@ const Main = () => {
 
   return (
     <main>
-      <Welcome />
+      {loading ? null : <Home />}
     </main>
   );
 }
