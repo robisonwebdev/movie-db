d6e6474219dc845b3dbffe9a9a912f1f75e744e9
robisonwebdev <david@robisonweb.dev>
2021-11-16 13:27:29 -0700

    Imported useCallback, useEffect, useState, and axios. Setup useState for apiKey, loading, popularMovies, and popularShows. Setup fetchData function to get api data on popular shows and popular movies, and set them to correct state. Setup useEffect to call fetchData. Setup temp useEffect for console.log when loading is false.

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 import Welcome from './Welcome';
 import Discover from './Discover';
 import '../../styles/home_page/Home.css';
 
 const Home = () => {
+    const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [loading, setLoading] = useState(true);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [popularShows, setPopularShows] = useState([]);
+
+    const fetchData = useCallback(() => {
+        const moviesAPI = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+        const showsAPI = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+        const getMoviesData = axios.get(moviesAPI);
+        const getShowsData = axios.get(showsAPI);
+
+        setLoading(true);
+
+        axios
+            .all([getMoviesData, getShowsData])
+            .then(axios.spread((...allData) => {
+                const moviesData = allData[0].data;
+                const showsData = allData[1].data;
+
+                setPopularMovies(moviesData);
+                setPopularShows(showsData);
+                setLoading(false);
+            }))
+            .catch(err => console.log(err))
+    }, [apiKey]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    useEffect(() => {
+        if (!loading) {
+            console.log('Movies', popularMovies);
+            console.log('Shows', popularShows);
+        }
+    }, [loading, popularShows, popularMovies]);
+
     return (
         <div className='home'>
             <Welcome />
