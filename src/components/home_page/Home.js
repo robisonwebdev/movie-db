de37229665c4797cb9934066c21a9f4c43d7dbaf
robisonwebdev <david@robisonweb.dev>
2021-11-16 15:48:26 -0700

    Setup useState for popularInTheaters. Setup fetchData to get theaterData and set it to popularInTheaters. Passed popularMovies and popularShows to Discover Component.

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -9,24 +9,29 @@ const Home = () => {
     const [loading, setLoading] = useState(true);
     const [popularMovies, setPopularMovies] = useState([]);
     const [popularShows, setPopularShows] = useState([]);
+    const [popularInTheaters, setPopularInTheaters] = useState([]);
 
     const fetchData = useCallback(() => {
         const moviesAPI = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
         const showsAPI = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
+        const theatersAPI = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&sort_by=popularity.desc`;
 
         const getMoviesData = axios.get(moviesAPI);
         const getShowsData = axios.get(showsAPI);
+        const getTheathersData = axios.get(theatersAPI);
 
         setLoading(true);
 
         axios
-            .all([getMoviesData, getShowsData])
+            .all([getMoviesData, getShowsData, getTheathersData])
             .then(axios.spread((...allData) => {
                 const moviesData = allData[0].data;
                 const showsData = allData[1].data;
+                const theatersData = allData[2].data;
 
                 setPopularMovies(moviesData);
                 setPopularShows(showsData);
+                setPopularInTheaters(theatersData);
                 setLoading(false);
             }))
             .catch(err => console.log(err))
@@ -40,14 +45,17 @@ const Home = () => {
         if (!loading) {
             console.log('Movies', popularMovies);
             console.log('Shows', popularShows);
+            console.log('Theater', popularInTheaters);
         }
-    }, [loading, popularShows, popularMovies]);
+    }, [loading, popularShows, popularMovies, popularInTheaters]);
 
     return (
         <div className='home'>
             <Welcome />
             <Discover
                 className='whatsPopular'
+                movies={loading ? null : popularMovies}
+                shows={loading ? null : popularShows}
                 selectors={['On TV', 'For Rent', 'In Theaters']}
                 title="What's Popular"
             />
