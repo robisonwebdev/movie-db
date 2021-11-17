e4f9303fae9f20c5e118fe61b4bd7d5116d9bdae
robisonwebdev <david@robisonweb.dev>
2021-11-17 15:56:20 -0700

    Changed movie prop to media. Added two new variables airDate and name. Changed h4 to take name or title. Changed ShowDate value to take airDate or releaseDate.

--- src/components/home_page/Card.js
+++ src/components/home_page/Card.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ShowDate from './ShowDate';
 import '../../styles/home_page/Card.css';
 
-const Card = ({ movie }) => {
-    const poster = `https://image.tmdb.org/t/p/w185/${movie.poster_path}`;
-    const title = movie.title;
-    const releaseDate = movie.release_date;
+const Card = ({ media }) => {
+    const airDate = media.first_air_date;
+    const name = media.name
+    const poster = `https://image.tmdb.org/t/p/w185/${media.poster_path}`;
+    const title = media.title;
+    const releaseDate = media.release_date;
 
     return (
         <div className='card'>
-            <img src={poster} alt={`${title} poster`} />
+            <img src={poster} alt={`${name || title} poster`} />
             <div className='cardContent'>
-                <h4>{title}</h4>
-                <ShowDate value={releaseDate} />
+                <h4>{name || title}</h4>
+                <ShowDate value={airDate || releaseDate} />
             </div>            
         </div>
     )
