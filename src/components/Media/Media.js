bad304c4eb33993e76ff5107e6571632d44f6dbe
robisonwebdev <david@robisonweb.dev>
2021-11-19 11:48:33 -0700

    Changed DisplayPage.css import path to Media.css Changed return div className to media.

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import FilterPanel from './FilterPanel';
 import ResultPanel from './ResultPanel';
-import '../../styles/DisplayPage.css';
+import '../../styles/Media/Media.css';
 
 const Media = ({ movies, title }) => {
     const [sortBy, setSortBy] = useState('pop_des');
 
     return (
-        <div className='displayPage'>
+        <div className='media'>
             <h2>{title || 'Title'}</h2>
             <div className='results'>
                 <FilterPanel setSortBy={setSortBy} />
