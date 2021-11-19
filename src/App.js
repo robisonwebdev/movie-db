b756234c8b073071e815a8517b79e0df23cb8901
robisonwebdev <david@robisonweb.dev>
2021-11-19 12:23:52 -0700

    Changed Footer import path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Navigation/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import './styles/App.css';
 
 const App = () => {
