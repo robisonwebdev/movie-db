e76166659865d83bfa82e65b23f7d7f762fc8459
robisonwebdev <david@robisonweb.dev>
2021-11-20 15:29:28 -0700

    Imported Link. Added Link components to Logo and both NavLink components.

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavLink from './NavLink';
 import Logo from './Logo';
 import '../../styles/Navigation/Navbar.css';
@@ -6,10 +7,10 @@ import '../../styles/Navigation/Navbar.css';
 const Nav = () => {
     return (
         <nav>
-            <Logo />
+            <Link to='/'><Logo /></Link>
             <ul>
-                <NavLink linkName='Movies' />
-                <NavLink linkName='TV Shows' />
+                <Link to='/movies'><NavLink linkName='Movies' /></Link>
+                <Link to='/shows'><NavLink linkName='TV Shows' /></Link>                
             </ul>
         </nav>
     )
