7b0a2bb28edcd24d8efde221f80cc7e909d39320
robisonwebdev <david@robisonweb.dev>
2021-11-20 15:26:14 -0700

    Imported BrowserRouter. Added BrowserRouter to render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
