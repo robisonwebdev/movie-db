9d6a021a427908b2a732228e1c628291916f10c0
robisonwebdev <david@robisonweb.dev>
2021-11-23 13:23:43 -0700

    Removed movie prop, and destructured format, get, and page. Added apiKey, loading, and media useState. Setup fetchData useCallback function to get data and set it to state. Setup useEffect to run fetchData. Changed ResultPanel movie prop to media and passed it media state. Imported useCallback and useEffect.

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import FilterPanel from './FilterPanel';
 import ResultPanel from './ResultPanel';
 import '../../styles/Media/Media.css';
+import axios from 'axios';
 
-const Media = ({ movies, title }) => {
+const Media = ({ format, get, page, title }) => {
+    const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [loading, setLoading] = useState(true);
+    const [media, setMedia] = useState([]);
     const [sortBy, setSortBy] = useState('pop_des');
+
+    const fetchData = useCallback(() => {
+        const getAPI = ` https://api.themoviedb.org/3/${format}/${get}?api_key=${apiKey}&language=en-US&page=${page || '1'}`;
+
+        setLoading(true);
+
+        axios
+        .get(getAPI)
+        .then(res => {
+            setMedia(res.data.results);
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+
+    }, [apiKey, format, get, page]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData])
 
     return (
         <div className='media'>
             <h2>{title || 'Title'}</h2>
             <div className='results'>
                 <FilterPanel setSortBy={setSortBy} />
-                <ResultPanel movies={movies} sortBy={sortBy} />
+                <ResultPanel media={media} sortBy={sortBy} />
             </div>
         </div>
     )
