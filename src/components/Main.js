71b8550c2919595b59ad22d385865e1616710d58
robisonwebdev <david@robisonweb.dev>
2021-11-23 13:31:11 -0700

    Removed useEffect, useState, axios, and Overview.js imports. Removed apiKey, loading, and popularMovies state. Removed both useEffects. Removed movies prop from all Media components, and replaced with format and get props.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,56 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
-import axios from 'axios';
 import Home from './Home/Home';
-import Overview from './Overview/Overview';
 import Media from './Media/Media';
 import '../styles/Main.css';
 
 const Main = () => {
-  const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
-  const [loading, setLoading] = useState(true);
-  const [popularMovies, setPopularMovies] = useState([]);
-
-  // Get popular movies on load
-  useEffect(() => {
-    setLoading(true);
-
-    axios
-      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&sort_bypage=1`)
-      .then(res => {
-        setPopularMovies(res.data.results)
-        setLoading(false);
-      })
-      .catch(err => console.log(err))
-    
-  }, [apiKey]);
-
-  // Temp useEffect
-  useEffect(() => {
-    if (!loading) {
-      console.log(popularMovies);
-    }
-  }, [loading, popularMovies]);
-
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
 
         <Route path='/movies' element={<Layout />}>
-          <Route path='/movies/popular' element={<Media movies={popularMovies} title='Popular Movies' />} />
-          <Route path='/movies/now_playing' element={<Media movies={popularMovies} title='Now Playing Movies' />} />
-          <Route path='/movies/upcoming' element={<Media movies={popularMovies} title='Upcoming Movies' />} />
-          <Route path='/movies/top_rated' element={<Media movies={popularMovies} title='Top Rated Movies' />} />
-          <Route index element={<Media movies={popularMovies} title='Popular Movies' />} />
+          <Route path='/movies/popular' element={<Media format='movie' get='popular' title='Popular Movies' />} />
+          <Route path='/movies/now_playing' element={<Media format='movie' get='now_playing' title='Now Playing Movies' />} />
+          <Route path='/movies/upcoming' element={<Media format='movie' get='upcoming' title='Upcoming Movies' />} />
+          <Route path='/movies/top_rated' element={<Media format='movie' get='top_rated' title='Top Rated Movies' />} />
+          <Route index element={<Media format='movie' get='popular' title='Popular Movies' />} />
         </Route>
 
         <Route path='/shows' element={<Layout />}>
-          <Route path='/shows/popular' element={<Media movies={popularMovies} title='Popular TV Shows' />} />
-          <Route path='/shows/airing_today' element={<Media movies={popularMovies} title='TV Shows Airing Today' />} />
-          <Route path='/shows/on_tv' element={<Media movies={popularMovies} title='Currently Airing TV Shows' />} />
-          <Route path='/shows/top_rated' element={<Media movies={popularMovies} title='Top Rated TV Shows' />} />
-          <Route index element={<Media movies={popularMovies} title='Popular TV Shows' />} />
+          <Route path='/shows/popular' element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
+          <Route path='/shows/airing_today' element={<Media format='tv' get='airing_today' title='TV Shows Airing Today' />} />
+          <Route path='/shows/on_tv' element={<Media format='tv' get='on_the_air' title='Currently Airing TV Shows' />} />
+          <Route path='/shows/top_rated' element={<Media format='tv' get='top_rated' title='Top Rated TV Shows' />} />
+          <Route index element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
         </Route>
 
         <Route path='/person' element={<Layout />}>
