2fe9e89248523429b78f262eff8796b775002271
robisonwebdev <david@robisonweb.dev>
2021-11-26 14:43:05 -0700

    Fixed Route structure.

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,31 +11,31 @@ const Main = () => {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
+      </Route>
 
-        <Route path='/movies' element={<Layout />}>
-          <Route path='/movies/popular' element={<Media format='movie' get='popular' title='Popular Movies' />} />
-          <Route path='/movies/now_playing' element={<Media format='movie' get='now_playing' title='Now Playing Movies' />} />
-          <Route path='/movies/upcoming' element={<Media format='movie' get='upcoming' title='Upcoming Movies' />} />
-          <Route path='/movies/top_rated' element={<Media format='movie' get='top_rated' title='Top Rated Movies' />} />
-          <Route index element={<Media format='movie' get='popular' title='Popular Movies' />} />
-        </Route>
+      <Route path='/movies' element={<Layout />}>
+        <Route path='/movies/popular' element={<Media format='movie' get='popular' title='Popular Movies' />} />
+        <Route path='/movies/now_playing' element={<Media format='movie' get='now_playing' title='Now Playing Movies' />} />
+        <Route path='/movies/upcoming' element={<Media format='movie' get='upcoming' title='Upcoming Movies' />} />
+        <Route path='/movies/top_rated' element={<Media format='movie' get='top_rated' title='Top Rated Movies' />} />
+        <Route index element={<Media format='movie' get='popular' title='Popular Movies' />} />
+      </Route>
 
-        <Route path='/movie' element={<Layout />}>
-          <Route path='/movie/:id' element={<Overview />} />
-        </Route>
+      <Route path='/movie' element={<Layout />}>
+        <Route path='/movie/:id' element={<Overview />} />
+      </Route>
 
-        <Route path='/shows' element={<Layout />}>
-          <Route path='/shows/popular' element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
-          <Route path='/shows/airing_today' element={<Media format='tv' get='airing_today' title='TV Shows Airing Today' />} />
-          <Route path='/shows/on_tv' element={<Media format='tv' get='on_the_air' title='Currently Airing TV Shows' />} />
-          <Route path='/shows/top_rated' element={<Media format='tv' get='top_rated' title='Top Rated TV Shows' />} />
-          <Route index element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
-        </Route>
+      <Route path='/shows' element={<Layout />}>
+        <Route path='/shows/popular' element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
+        <Route path='/shows/airing_today' element={<Media format='tv' get='airing_today' title='TV Shows Airing Today' />} />
+        <Route path='/shows/on_tv' element={<Media format='tv' get='on_the_air' title='Currently Airing TV Shows' />} />
+        <Route path='/shows/top_rated' element={<Media format='tv' get='top_rated' title='Top Rated TV Shows' />} />
+        <Route index element={<Media format='tv' get='popular' title='Popular TV Shows' />} />
+      </Route>
 
-        <Route path='/person' element={<Layout />}>
-          <Route path='/person/popular_people' element={<People />} />
-          <Route index element={<People />} />
-        </Route>
+      <Route path='/person' element={<Layout />}>
+        <Route path='/person/popular_people' element={<People />} />
+        <Route index element={<People />} />
       </Route>
     </Routes>
   );
