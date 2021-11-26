286ff9893bba964f67cfaa79cf69c36c92d6c187
robisonwebdev <david@robisonweb.dev>
2021-11-26 14:23:46 -0700

    Changed image variable, to getImage function, that checks for profile_path, and returns address based on result.

--- src/components/People/Card.js
+++ src/components/People/Card.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../../styles/People/Card.css';
 
-const Card = ({ person }) => {
-    const image = `https://image.tmdb.org/t/p/w300/${person.profile_path}`;
+const Card = ({ person }) => {    
+    const getImage = () => {
+        if (person.profile_path === null) {
+            return `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg`
+        }
+
+        return `https://image.tmdb.org/t/p/w235_and_h235_face/${person.profile_path}`;
+    }
 
     return (
         <div className='person'>
             {console.log(person)}
-            <img src={image} alt={person.name} />
+            <img src={getImage()} alt={person.name} />
             <div className='personInfo'>
                 <h3>{person.name}</h3>
                 <p>{'movies'}</p>
