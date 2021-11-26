eeef24810f1e398350158dfc76e48acff855b0f6
robisonwebdev <david@robisonweb.dev>
2021-11-26 13:52:13 -0700

    Imported Card.css Destrucutred person. Setup image variable to get person's image. Gave return div a className of person. Inside return div, setup img tag and added div with className of personInfo. Inside personInfo, added h3 set to person.name and p tag.

--- src/components/People/Card.js
+++ src/components/People/Card.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../../styles/People/Card.css';
 
-const Card = () => {
+const Card = ({ person }) => {
+    const image = `https://image.tmdb.org/t/p/w300/${person.profile_path}`;
+
     return (
-        <div>
-            Card
+        <div className='person'>
+            {console.log(person)}
+            <img src={image} alt={person.name} />
+            <div className='personInfo'>
+                <h3>{person.name}</h3>
+                <p>{'movies'}</p>
+            </div>
         </div>
     )
 };
