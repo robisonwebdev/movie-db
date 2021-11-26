a989017c23e82b77b2aabd708b6a9756ed31c606
robisonwebdev <david@robisonweb.dev>
2021-11-25 18:17:08 -0700

    Imported useCallback, useEffect, useState, and axios. Setup state for apiKey, loading, and popularPeople. Setup fetchData to get popular people api. Setup useEffect to call fetchData.

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react/cjs/react.development';
 
 const People = () => {
+    const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [loading, setLoading] = useState(true);
+    const [popularPeople, setPopularPeople] = useState([]);
+
+    const fetchData = useCallback((page) => {
+        const peopleAPI = ` https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`;
+
+        setLoading(true);
+
+        axios
+        .get(peopleAPI)
+        .then(res => {
+            setPopularPeople(res.data.results);
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+    }, [apiKey]);
+
+    useEffect(() => {
+        fetchData('1');
+    }, [fetchData]);
+
     return (
         <div>
-            People
+            {loading ? null : console.log(popularPeople)}
         </div>
     )
 };
