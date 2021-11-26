0fbb8b993341959079245e399ee252bd49e0b98e
robisonwebdev <david@robisonweb.dev>
2021-11-26 11:42:19 -0700

    Imported Card.js Setup buildCard variable to map through popularPeople and return each person as Card component. Changed return div to section, and gave it a className of popularPeople. Inside return sections, added h2 and div with className of people_content. Inside people_Content, setup ternery with buildCards.

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useCallback, useState } from 'react';
 import axios from 'axios';
+import Card from './Card';
 import '../../styles/People/People.css';
 
 const People = () => {
     const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
     const [loading, setLoading] = useState(true);
     const [popularPeople, setPopularPeople] = useState([]);
+
+    const buildCards = popularPeople.map(person => {
+        return <Card key={person.id} person={person} />
+    });
 
     const fetchData = useCallback((page) => {
         const peopleAPI = ` https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`;
@@ -26,9 +31,12 @@ const People = () => {
     }, [fetchData]);
 
     return (
-        <div>
-            {loading ? null : console.log(popularPeople)}
-        </div>
+        <section className='popularPeople'>
+            <h2>Popular People</h2>
+            <div className='people_content'>
+                {loading ? null : buildCards}
+            </div>
+        </section>
     )
 };
 
