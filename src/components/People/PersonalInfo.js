2ca159bdf95a2c8dc16fbedec94d22806cda4c04
robisonwebdev <david@robisonweb.dev>
2021-11-29 18:28:23 -0700

    Imported PersonalInfo.css Gave return div a className of .personalInfo

---
+++ src/components/People/PersonalInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/People/PersonalInfo.css';
+
+const PersonalInfo = () => {
+    return (
+        <div className='personalInfo'>
+            PersonalInfo
+        </div>
+    )
+};
+
+export default PersonalInfo;
