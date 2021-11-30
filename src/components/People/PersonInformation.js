0b45982ed4dd8b3a48cd6af4812ee7c5d3d2e386
robisonwebdev <david@robisonweb.dev>
2021-11-29 18:30:15 -0700

    Imported PersonalInfo.js Fixed return div className typo. Added img and PersonalInfo component to personInformation_left.

--- src/components/People/PersonInformation.js
+++ src/components/People/PersonInformation.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import PersonalInfo from './PersonalInfo';
 import '../../styles/People/PersonInformation.css';
 
 const PersonInformation = () => {
     return (
-        <div className='personInformaiton'>
-            <div className='personInformation_left'>Left</div>
+        <div className='personInformation'>
+            <div className='personInformation_left'>
+                <img src='#' alt='#' />
+                <PersonalInfo />
+            </div>
             <div className='personInformation_right'>Right</div>
         </div>
     )
