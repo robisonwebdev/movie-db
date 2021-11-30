b94bbbd53d2a92bf5df2e17014337b7d39fe7655
robisonwebdev <david@robisonweb.dev>
2021-11-29 18:54:58 -0700

    Added temp img src.

--- src/components/People/PersonInformation.js
+++ src/components/People/PersonInformation.js
@@ -6,7 +6,7 @@ const PersonInformation = () => {
     return (
         <div className='personInformation'>
             <div className='personInformation_left'>
-                <img src='#' alt='#' />
+                <img src='https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sOi4UZxflV07E7QXUaJTHROGlPU.jpg' alt='#' />
                 <PersonalInfo />
             </div>
             <div className='personInformation_right'>Right</div>
