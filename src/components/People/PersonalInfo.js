9c3a3255812c3f513bfa91fa8c3854a5e6dde291
robisonwebdev <david@robisonweb.dev>
2021-11-29 18:37:37 -0700

    Created PersonalInfoSection component inside file. Called PersonalInfoSection inside .personalInfo return div, with some temp values

--- src/components/People/PersonalInfo.js
+++ src/components/People/PersonalInfo.js
@@ -4,9 +4,24 @@ import '../../styles/People/PersonalInfo.css';
 const PersonalInfo = () => {
     return (
         <div className='personalInfo'>
-            PersonalInfo
+            <h2>Personal Info</h2>
+            <PersonalInfoSection info={'Temp Text'} title='Known For' />
+            <PersonalInfoSection info={'Temp Text'} title='Known Credits' />
+            <PersonalInfoSection info={'Temp Text'} title='Gender' />
+            <PersonalInfoSection info={'Temp Text'} title='Birthday' />
+            <PersonalInfoSection info={'Temp Text'} title='Place of Birth' />
+            <PersonalInfoSection info={'Temp Text'} title='Also Known As' />
         </div>
     )
 };
 
 export default PersonalInfo;
+
+const PersonalInfoSection = ({ info, title}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>{info}</p>
+        </div>
+    );
+}
