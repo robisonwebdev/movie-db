b7bc51bd4e13562930c1951a74f1fefe71118817
robisonwebdev <david@robisonweb.dev>
2021-11-30 16:31:18 -0700

    Destructured person. Setup function to return person's birthday and age. Removed temp text from Known For, and replaced with data from person prop. Removed temp text from Birthday, and replaced with getBirthDay.

--- src/components/People/PersonalInfo.js
+++ src/components/People/PersonalInfo.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../../styles/People/PersonalInfo.css';
 
-const PersonalInfo = () => {
+const PersonalInfo = ({ person }) => {
+    const getBirthDay = () => {
+        const today = new Date();
+        const birthDate = new Date(person.birthday);
+        const age = today.getFullYear() - birthDate.getFullYear();
+
+        return `${person.birthday} (${age} years old)`;
+    }
+
     return (
         <div className='personalInfo'>
             <h3>Personal Info</h3>
-            <PersonalInfoSection info={'Temp Text'} title='Known For' />
+            <PersonalInfoSection info={person.known_for_department} title='Known For' />
             <PersonalInfoSection info={'Temp Text'} title='Known Credits' />
             <PersonalInfoSection info={'Temp Text'} title='Gender' />
-            <PersonalInfoSection info={'Temp Text'} title='Birthday' />
+            <PersonalInfoSection info={getBirthDay()} title='Birthday' />
             <PersonalInfoSection info={'Temp Text'} title='Place of Birth' />
             <PersonalInfoSection info={'Temp Text'} title='Also Known As' />
         </div>
