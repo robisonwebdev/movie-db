6f539aa4dea3a5d6d471c7b55dbf0a672eddea74
robisonwebdev <david@robisonweb.dev>
2021-11-29 18:11:51 -0700

    Setup initial functional PersonInformation component.

---
+++ src/components/People/PersonInformation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PersonInformation = () => {
+    return (
+        <div>
+            Person Information
+        </div>
+    )
+};
+
+export default PersonInformation;
