3fa5e14f429657f287a6b71d76aa8b966cb72f3d
robisonwebdev <david@robisonweb.dev>
2021-11-30 17:53:48 -0700

    Imported useCallback, useEffect, useState, and useParams. Setup state for apiKey, loading, image, and person. Setup id using useParams. Setup fetchData function to get data on person, based on id. Setup useEffect to call fetchData. Moved PersonalInfo into an ternery and passed it person.

--- src/components/People/PersonInformation.js
+++ src/components/People/PersonInformation.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import PersonalInfo from './PersonalInfo';
 import '../../styles/People/PersonInformation.css';
+import axios from 'axios';
 
 const PersonInformation = () => {
+    const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [loading, setLoading] = useState(true);
+    const [image, setImage] = useState();
+    const [person, setPerson] = useState([]);
+
+    const { id } = useParams();
+
+    const fetchData = useCallback(() => {
+        const personAPI = ` https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=en-US`;
+
+        setLoading(true);
+
+        axios
+        .get(personAPI)
+        .then(res => {
+            setImage(res.data.profile_path);
+            setPerson(res.data);
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+    }, [apiKey, id]);
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
     return (
         <div className='personInformation'>
             <div className='personInformation_left'>
-                <img src='https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sOi4UZxflV07E7QXUaJTHROGlPU.jpg' alt='#' />
-                <PersonalInfo />
+                <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${image}`} alt='#' />
+                {loading ? null : <PersonalInfo person={person} />}
             </div>
             <div className='personInformation_right'>Right</div>
         </div>
