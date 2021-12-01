23eb464269b67f2f5d6a23713d6bbc67ced8be4c
robisonwebdev <david@robisonweb.dev>
2021-11-30 19:47:54 -0700

    Setup state for combinedCredits. Reworked fetchData to get two APIs. Passed combinedCredits to PersonalInfo component.

--- src/components/People/PersonInformation.js
+++ src/components/People/PersonInformation.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const PersonInformation = () => {
     const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
+    const [combinedCredits, setCombinedCredits] = useState([]);
     const [loading, setLoading] = useState(true);
     const [image, setImage] = useState();
     const [person, setPerson] = useState([]);
@@ -13,17 +14,25 @@ const PersonInformation = () => {
     const { id } = useParams();
 
     const fetchData = useCallback(() => {
-        const personAPI = ` https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=en-US`;
+        const combinedCredits_API = `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${apiKey}&language=en-US`;
+        const person_API = `https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=en-US`;
+
+        const getCombinedCredits_Data = axios.get(combinedCredits_API);
+        const getPerson_Data = axios.get(person_API);
 
         setLoading(true);
 
         axios
-        .get(personAPI)
-        .then(res => {
-            setImage(res.data.profile_path);
-            setPerson(res.data);
+        .all([getCombinedCredits_Data, getPerson_Data])
+        .then(axios.spread((...all_Data) => {
+            const combinedCredits_Data = all_Data[0].data;
+            const person_Data = all_Data[1].data;
+
+            setCombinedCredits(combinedCredits_Data);
+            setImage(person_Data.profile_path);
+            setPerson(person_Data);
             setLoading(false);
-        })
+        }))
         .catch(err => console.log(err))
     }, [apiKey, id]);
 
@@ -34,8 +43,8 @@ const PersonInformation = () => {
     return (
         <div className='personInformation'>
             <div className='personInformation_left'>
-                <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${image}`} alt='#' />
-                {loading ? null : <PersonalInfo person={person} />}
+                {loading ? null : <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${image}`} alt='#' />}
+                {loading ? null : <PersonalInfo combinedCredits={combinedCredits} person={person} />}
             </div>
             <div className='personInformation_right'>Right</div>
         </div>
