e76d1eb8d1aa6b111d34dcb9e198d26a9266bdf8
robisonwebdev <david@robisonweb.dev>
2021-12-03 10:24:02 -0700

    Imported KnownForCard.css Added alt to img

--- src/components/People/KnownForCard.js
+++ src/components/People/KnownForCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../../styles/People/KnownForCard.css';
 
 function KnownForCard({ image, title }) {
     return (
         <div className='knownForCard'>
-            <img src={`https://image.tmdb.org/t/p/w154/${image}`} />
+            <img src={`https://image.tmdb.org/t/p/w154/${image}`} alt={`${title} poster`} />
             <p>{title}</p>
         </div>
     )
