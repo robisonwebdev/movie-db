a6e106b2becaf57c7024f34151de5cfc59ac0561
robisonwebdev <david@robisonweb.dev>
2021-12-02 18:38:36 -0700

    Destructured knownFor. Setup topEight variable to get the top eight objects with highest vote_count. Setup initial mapTopEight variable to return each object in a p tag. Called mapTopEight in knowFor_content.

--- src/components/People/KnownFor.js
+++ src/components/People/KnownFor.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../../styles/People/KnownFor.css';
 
-const KnownFor = () => {
+const KnownFor = ({ knownFor }) => {
+    const topEight = knownFor.cast.sort((a, b) => parseFloat(b.vote_count) - parseFloat(a.vote_count)).slice(0, 8);
+    const mapTopEight = topEight.map(obj => {
+        return <p>{obj.title}</p>
+    })
+
     return (
         <div className='knownFor'>
             <h3>Known For</h3>
-            <div className='knownFor_content'></div>
+            <div className='knownFor_content'>
+                {mapTopEight}
+            </div>
         </div>
     )
 };
