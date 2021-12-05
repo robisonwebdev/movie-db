b4d747524f7ac763d1213f2d10cf317f512791ce
robisonwebdev <david@robisonweb.dev>
2021-12-05 08:35:04 -0700

    Imported useState. Destructured credits. Setup state for department and actingCredits. Changed title prop to department.

--- src/components/People/CreditsList.js
+++ src/components/People/CreditsList.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CreditsList = () => {
+const CreditsList = ({ credits }) => {
+    const [department, setDepartment] = useState('Acting');
+    const [actingCredits, setActingCredits] = useState([]);
+
     return (
         <div className='creditsList'>
             <div className='credits_Header'>
-                <h3>{title}</h3>
+                <h3>{department}</h3>
                 <div className='credit_Filters'></div>
             </div>
             <div className='credits_body'></div>
