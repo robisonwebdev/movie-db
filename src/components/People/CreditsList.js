1d04dcc5c1386a5ec8a4e76ea7bcd208b260e829
robisonwebdev <david@robisonweb.dev>
2021-12-07 13:34:33 -0700

    Setup useEffect that groups credits.cast by year, then reverses array, and sets to setActingCredits. Created vaiable to build timeline from actingCredits and mapped through it. Returns CreditGroup component. Inside CreditItem component, setup itemYear function to return only the year of item.

--- src/components/People/CreditsList.js
+++ src/components/People/CreditsList.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CreditsList = ({ credits }) => {
     const [department, setDepartment] = useState('Acting');
     const [actingCredits, setActingCredits] = useState([]);
+
+    useEffect(() => {
+        const groupByYear = credits.cast.reduce((r, a) => {
+            r[new Date(a.release_date || a.first_air_date).getUTCFullYear()] = [...r[new Date(a.release_date || a.first_air_date).getUTCFullYear()] || [], a];
+            return r;
+        }, {});
+
+        const newArray = Object.keys(groupByYear).map((key) => groupByYear[key]).reverse();
+
+        setActingCredits(newArray);
+    }, []);
+
+    const buildTimeline = actingCredits.map(arr =>{
+        return <CreditGroup key={Math.floor(Math.random() * 99999)} group={arr} />
+    });
 
     return (
         <div className='creditsList'>
@@ -10,25 +25,50 @@ const CreditsList = ({ credits }) => {
                 <h3>{department}</h3>
                 <div className='credit_Filters'></div>
             </div>
-            <div className='credits_body'></div>
+            <div className='credits_body'>
+                {buildTimeline}
+            </div>
         </div>
     );
 };
 
-const CreditGroup = () => {
+const CreditGroup = ({ group }) => {
+    const buildItems = group.map(item => <CreditItem key={item.id} item={item} />);
+
     return (
         <div className='creditGroup'>
-
+            {buildItems}
         </div>
     );
 };
 
-const CreditItem = () => {
+const CreditItem = ({ item }) => {
+    const itemYear = () => {
+        let itemDate;
+        let itemYear;
+
+        if (item.release_date === "" || item.first_air_date === "") {
+            return "-";
+        }
+
+        if (item.release_date) {
+            itemDate = new Date(item.release_date);
+            itemYear = itemDate.getUTCFullYear();
+        }
+    
+        if (item.first_air_date) {
+            itemDate = new Date(item.first_air_date);
+            itemYear = itemDate.getUTCFullYear();
+        }
+
+        return itemYear;
+    }
+
     return (
         <div className='creditItem'>
-            <p>{`year`}</p>
+            <p>{itemYear()}</p>
             <i />
-            <p>{`media title`}</p>
+            <p>{item.title || item.name}</p>
         </div>
     );
 }
