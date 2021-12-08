bcad554d816d1ae29cbcd418b2629fe15ff6bdfb
robisonwebdev <david@robisonweb.dev>
2021-12-07 18:47:23 -0700

    Imported Link. In getImage function added alt of person.name, and className of noImage for if statement return. Added alt of person.name for default return. Setup Link in return div on getImage and h3.

--- src/components/People/Card.js
+++ src/components/People/Card.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/People/Card.css';
 
 const Card = ({ person }) => {    
     const getImage = () => {
         if (person.profile_path === null) {
-            return `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg`
+            return  <img
+                        src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg'
+                        alt={person.name}
+                        className='noImage'
+                    />
         }
 
-        return `https://image.tmdb.org/t/p/w235_and_h235_face/${person.profile_path}`;
+        return <img src={`https://image.tmdb.org/t/p/w235_and_h235_face/${person.profile_path}`} alt={person.name} />
     }
 
     return (
         <div className='person'>
-            {console.log(person)}
-            <img src={getImage()} alt={person.name} />
+            <Link to={`/people/person/${person.id}`}>{getImage()}</Link>
             <div className='personInfo'>
-                <h3>{person.name}</h3>
+                <Link to={`/people/person/${person.id}`}><h3>{person.name}</h3></Link>
                 <p>{'movies'}</p>
             </div>
         </div>
