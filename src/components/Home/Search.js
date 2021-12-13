d8ce37739314f482697f6dfef4ecdb0fc6d172bd
robisonwebdev <david@robisonweb.dev>
2021-12-12 17:27:10 -0700

    Imported useState and useNavigate. Setup state for value, with an initial value of an empty string. Setup navigate. Setup handleChange function and called it on input's onChange. On input's value, set it to value state. Added nevigate to handleSubmit, which will switch page to search page.

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import '../../styles/Home/Search.css';
 
 const Search = () => {
+    const [value, setValue] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        navigate(`search/${value}`);
     }
 
     return (
         <form className='search' onSubmit={handleSubmit}>
-            <input type='text' placeholder='Search for a movie, tv show, person......' />
+            <input type='text' value={value} onChange={handleChange} placeholder='Search for a movie, tv show, person......' />
             <button type='submit'>Search</button>
         </form>
     )
