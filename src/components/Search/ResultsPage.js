1085d033c84e955485059e827bbe0d2eb2032bc9
robisonwebdev <david@robisonweb.dev>
2021-12-12 17:24:17 -0700

    Imported useCallback, useState, and useParams. Setup multiple sets of state. Setup variable for useParams. Setup initial fetchData.

--- src/components/Search/ResultsPage.js
+++ src/components/Search/ResultsPage.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState} from 'react';
+import { useParams } from 'react-router';
 import '../../styles/Search/ResultsPage.css';
 
 const ResultsPage = () => {
+    const { searchParam } = useParams();
+    const [collections, setCollections] = useState([]);
+    const [companies, setComponies] = useState([]);
+    const [keywords, setKeywords] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [networks, setNetworks] = useState([]);
+    const [people, setPeople] = useState([]);
+    const [shows, setShows] = useState([]);
+
+    const fetchData = useCallback(() => {
+
+    })
+
     return (
         <section className='results_Page'>
-            Results Page
+            Results
         </section>
     )
 };
