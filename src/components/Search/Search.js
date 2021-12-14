84434e206c237ddd3603fc76052b3d8d1a20fbcd
robisonwebdev <david@robisonweb.dev>
2021-12-14 16:22:53 -0700

    Setup state for showCancelIcon, with an initial value of false. Setup two functions to handle showing cancel icon for mouse enter and mouse leave. Setup onMouseEnter and onMouseLeave on form. Added two icons within form. One once set within ternery.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,9 +3,26 @@ import '../../styles/Search/Search.css';
 
 const Search = () => {
     const [value, setValue] = useState('');
+    const [showCancelIcon, setShowCancelIcon] = useState(false);
 
     const handleChange = (event) => {
         setValue(event.target.value);
+    }
+
+    const handleMouseEnter = () => {
+        if (value === '') {
+            setShowCancelIcon(false);
+        } else {
+            setShowCancelIcon(true);
+        }
+    }
+
+    const handleMouseLeave = () => {
+        if (value === '') {
+            setShowCancelIcon(false);
+        } else {
+            setShowCancelIcon(false);
+        }
     }
 
     const handleSubmit = (event) => {
@@ -13,8 +30,15 @@ const Search = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} placeholder='Search for a movie, tv show, person...' type='text' value={value} />
+        <form onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onSubmit={handleSubmit}>
+            <i className='las la-search' />
+            <input
+                onChange={handleChange}                
+                placeholder='Search for a movie, tv show, person...'
+                type='text'
+                value={value}
+            />
+            {showCancelIcon ? <i className='las la-times' /> : null}
         </form>
     )
 };
