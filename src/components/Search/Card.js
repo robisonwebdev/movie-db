bfab1389eea34251f0c982779ad3c87e3ef11e4e
robisonwebdev <david@robisonweb.dev>
2021-12-15 16:58:03 -0700

    Destructured media. Setup getImage variable to get image. Gave return div a className of result_card. Inside .result_card, added img  and div with a className of .card_content. Set img src to getImage. Inside .card_content, added two div with classNames of .card_header and .card_body Inside .card_header, add h3 set to media.title and place for future date field. Inside .card_body, add p tag set to media.overview

--- src/components/Search/Card.js
+++ src/components/Search/Card.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ media }) => {
+    const getImage = `https://image.tmdb.org/t/p/w94_and_h141_bestv2/${media.poster_path}`;
+
     return (
-        <div>
-            Card
+        <div className='result_card'>
+            <img src={getImage} />
+            <div className='card_content'>
+                <div className='card_header'>
+                    <h3>{media.title}</h3>
+                    {/* Date */}
+                </div>
+                <div className='card_body'>
+                    <p>{media.overview}</p>
+                </div>
+            </div>
         </div>
     )
 };
