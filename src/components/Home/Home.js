5559938feb58f41a2cbedfe57abb04bb72b79eb8
robisonwebdev <david@robisonweb.dev>
2021-12-15 20:24:02 -0700

    Changed mediaAPI function to use useCallBack. Removed apiKey dependency from fetchData, and replaced with mediaAPI.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -45,7 +45,7 @@ const Home = () => {
         }
     }
 
-    const mediaAPI = (format, monetization, page, sort) => {
+    const mediaAPI = useCallback((format, monetization, page, sort) => {
         if (format === 'movie') {
             return `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=${sort || 'popularity.desc'}&include_adult=true&include_video=false&page=${page || '1'}&watch_region=US&&with_watch_monetization_types=${monetization || 'flatrate'}`;
         };
@@ -53,7 +53,7 @@ const Home = () => {
         if (format === 'tv') {
             return `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=en-US&sort_by=${sort || 'popularity.desc'}&page=${page || '1'}&timezone=America%2FNew_York&include_null_first_air_dates=false&watch_region=US&with_watch_monetization_types=${monetization || 'flatrate'}&with_status=0&with_type=0`;
         }
-    }
+    }, [apiKey]);
 
     const fetchData = useCallback(() => {    
         const streamingMoviesAPI = mediaAPI('movie', 'flatrate', 1, 'popularity.desc');
@@ -95,7 +95,7 @@ const Home = () => {
                 setLoading(false);
             }))
             .catch(err => console.log(err))
-    }, [apiKey]);
+    }, [mediaAPI]);
 
 useEffect(() => {
     fetchData();
