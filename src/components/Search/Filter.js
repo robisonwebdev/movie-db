390d846f0abf1433e025bd14f3b3db6b9c39d30f
robisonwebdev <david@robisonweb.dev>
2021-12-16 11:21:58 -0700

    Destructured onClick. Added onClick event to return div and passed it onClick prop.

--- src/components/Search/Filter.js
+++ src/components/Search/Filter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../styles/Search/Filter.css';
 
-const Filter = ({ number, title }) => {
+const Filter = ({ number, onClick, title }) => {
     return (
-        <div className='filter'>
+        <div className='filter' onClick={onClick}>
             <p className='filter_title'>{title}</p>
             <p className='filter_number'>{number}</p>
         </div>
