58ae5121d06245e38283ca093138c2a28eec1e52
robisonwebdev <david@robisonweb.dev>
2021-12-18 13:49:28 -0700

    Changed css import path. Changed getImage from variable to function. Changed return div to resultCard. Renamed Card component to ResultCard.

--- src/components/Search/ResultCard.js
+++ src/components/Search/ResultCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import '../../styles/Search/Card.css';
+import '../../styles/Search/ResultCard.css';
 
-const Card = ({ media }) => {
-    const getImage = `https://image.tmdb.org/t/p/w94_and_h141_bestv2/${media.poster_path}`;
+const ResultCard = ({ media }) => {
+    const getImage = () => {
+        const noImage = 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg';
+        const imageSize = (`w94_and_h141_bestv2` || 'w90_and_h90_face' || 'w180_and_h180_face');
+        const imagePath = (media.poster_path || media.profile_path);
+
+        if (imagePath === null) {
+            return noImage;
+        }
+
+        return `https://image.tmdb.org/t/p/${imageSize}/${imagePath}`;
+    };
 
     return (
-        <div className='result_card'>
-            <img src={getImage} alt={`${media.title} poster`} />
+        <div className='resultCard'>
+            <img src={getImage()} alt={`${media.title} poster`} />
             <div className='card_content'>
                 <div className='card_header'>
                     <h3>{media.title}</h3>
@@ -20,4 +30,4 @@ const Card = ({ media }) => {
     )
 };
 
-export default Card;
+export default ResultCard;
