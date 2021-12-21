7f676452c6d8662558a3f7e135bb1a4920292590
robisonwebdev <david@robisonweb.dev>
2021-12-21 14:17:30 -0700

    Imported MediaCard and PeopleCard. Setup two if statements to check what type prop contains and return certain component based on results.

--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import MediaCard from './MediaCard';
+import PeopleCard from './PeopleCard';
 
-const Results = () => {
+const Results = ({ media, type }) => {
+    // Media Type movie, shows, or collection
+    if (type === 'movie' || type==='shows' || type==='collection') {
+        const mapMedia = media.results?.map(item => {
+            return <MediaCard key={item.id} media={item} type={type} />
+        });
+
+        return (
+            <div className='results'>
+                {mapMedia}
+            </div>
+        );
+    }
+
+    if (type === 'people') {
+        const mapPeople = media.results?.map(person => {
+            return <PeopleCard key={person.id} person={person} />
+        });
+
+        return (
+            <div className='results'>
+                {mapPeople}
+            </div>
+        );
+    }
+
     return (
-        <div>
-            
+        <div className='results'>
+            <p>ERROR</p>
         </div>
     )
 };
