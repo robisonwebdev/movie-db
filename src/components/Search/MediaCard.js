64ab885ecbb282f4aa97d6c6fe559d85ad3ef77b
robisonwebdev <david@robisonweb.dev>
2021-12-20 22:40:35 -0700

    Imported MediaCard.css

--- src/components/Search/MediaCard.js
+++ src/components/Search/MediaCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/Search/MediaCard.css';
 
 const MediaCard = ({ media, type }) => {
     const getDate = () => {
