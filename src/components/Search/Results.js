82363c58c0ad4f374cd4965ba7e3f12abc3c6bfd
robisonwebdev <david@robisonweb.dev>
2021-12-22 19:42:26 -0700

    Imported KeywordCard. Setup if statement for type of keyword, mapped over keyword returning KeywordCard component.

--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import CompanyCard from './CompanyCard';
+import KeywordCard from './KeywordCard';
 import MediaCard from './MediaCard';
 import PeopleCard from './PeopleCard';
 import '../../styles/Search/Results.css';
-import CompanyCard from './CompanyCard';
 
 const Results = ({ media, type }) => {
     // Media Type movie, shows, or collection
@@ -43,6 +44,18 @@ const Results = ({ media, type }) => {
             </ul>
         );
     }
+
+    if (type === 'keywords') {
+        const mapKeywords = media.results?.map(keyword => {
+            return <KeywordCard key={keyword.id} keyword={keyword} />
+        });
+
+        return (
+            <ul className='result_list'>
+                {mapKeywords}
+            </ul>
+        );
+    };
 
     return (
         <div className='results'>
