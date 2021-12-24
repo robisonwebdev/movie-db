eff684ba6cf59fa8bc35261348fb3a2f2afad5aa
robisonwebdev <david@robisonweb.dev>
2021-12-24 16:13:18 -0700

    Changed h2 to p

--- src/components/Home/Welcome.js
+++ src/components/Home/Welcome.js
@@ -28,7 +28,7 @@ const Welcome = () => {
         <div className='welcome' style={image}>
             <div>
                 <h1>Welcome.</h1>
-                <h2>Millions of movies, TV shows and people to discover. Explore now.</h2>
+                <p>Millions of movies, TV shows and people to discover. Explore now.</p>
             </div>
             <Search />
         </div>
