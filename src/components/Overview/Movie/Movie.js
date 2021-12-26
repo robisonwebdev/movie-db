c25baba8169c815cdf8aabad754f5b3cc128a770
robisonwebdev <david@robisonweb.dev>
2021-12-26 10:48:09 -0700

    Changed className to movie_Overview

--- src/components/Overview/Movie/Movie.js
+++ src/components/Overview/Movie/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Movie = () => {
     return (
-        <div className='movieOverview'>
+        <div className='movie_Overview'>
             
         </div>
     )
