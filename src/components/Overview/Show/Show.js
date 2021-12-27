86d6ab137a27f2bbee8fb8e12e5abe2e6376b2a2
robisonwebdev <david@robisonweb.dev>
2021-12-27 15:39:48 -0700

    Imported useCallback, useEffect, useState, useParams, axios, api_key, and Header.js Setup initial code to get showData and display it on Header component.

--- src/components/Overview/Show/Show.js
+++ src/components/Overview/Show/Show.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import api_key from '../../../data/Key'
+import Header from '../Header';
 
 const Show = () => {
+    const [showData, setShowData] = useState([]);
+    const { id } = useParams();
+
+    const fetchData = useCallback(() => {
+        const showAPI = `https://api.themoviedb.org/3/tv/${id}?api_key=${api_key}&append_to_response=first_air_date,credits&language=en-US`;
+
+        axios
+        .get(showAPI)
+        .then(res => {
+            setShowData(res.data);
+        })
+        .catch(err => console.log(err))
+    }, [id]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
     return (
-        <div className='show_Overview'>
-            
+        <div className='show_overview'>
+            <Header media={showData} />
+            {console.log('showData', showData)}
         </div>
     )
 };
