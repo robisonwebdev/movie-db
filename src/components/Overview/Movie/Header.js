7420acfab5eb4086b5f2cc13c064cbe41c1e1f20
robisonwebdev <david@robisonweb.dev>
2021-12-28 10:03:05 -0700

    Imported Poster and Header.css. Setup variable to get backdropImage and to style backdrop. Copied code from Header.js in Overview folder and pasted here. Commented out components not setup.

--- src/components/Overview/Movie/Header.js
+++ src/components/Overview/Movie/Header.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Poster from '../Poster';
+import '../../../styles/Overview/Header.css';
 
-const Header = () => {
+const Header = ({ movie }) => {  
+    const backdropImage = `http://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
+    const backdropStyle = {
+        background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backdropImage})`,
+        backgroundPosition: 'center top',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    };
+
     return (
-        <div>
-            
+        <div className='overview_header' style={backdropStyle}>
+            <div className='overview_header_content'>
+                <Poster media={movie} />
+                <div className='overview_header_info'>
+                    {/* <Title
+                        media={media}
+                        format={format}
+                    /> */}
+                    {/* <Description
+                        description={movie.overview}
+                        tagline={movie.tagline}
+                    /> */}
+                    {/* <Crew
+                        credits={credits}
+                    /> */}
+                </div>
+            </div>
         </div>
     )
 };
