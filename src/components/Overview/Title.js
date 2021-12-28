b0c15b438d83c94d7f942af1707b90a771c75f7d
robisonwebdev <david@robisonweb.dev>
2021-12-27 21:53:15 -0700

    Changed movie and releaseDate props to media and format. Changed title to look for title and name from media. Commented out convertRuntime. Commented out getAndJoinGenres. Refactored getCertification. Changed apiDate in getDate function to also look for first_air_date. Setup if statement for format type of movie.

--- src/components/Overview/Title.js
+++ src/components/Overview/Title.js
@@ -1,43 +1,41 @@
 import React from 'react';
 import '../../styles/Overview/Title.css';
 
-const OverviewTitle = ({ movie, releaseDate }) => {
-    const title = movie.title;
+const OverviewTitle = ({ media, format }) => {
+    const title = media.title || media.name;
 
-    const convertRuntime = () => {
-        const runtime = movie.runtime;
-        const runtimeToHours = (runtime / 60);
-        const hours = Math.floor(runtimeToHours);
-        const minutes = Math.round((runtimeToHours - hours) * 60);
+    // const convertRuntime = () => {
+    //     const runtime = movie.runtime;
+    //     const runtimeToHours = (runtime / 60);
+    //     const hours = Math.floor(runtimeToHours);
+    //     const minutes = Math.round((runtimeToHours - hours) * 60);
 
-        return hours >=1 ? `${hours}h ${minutes}m` : `${minutes}m`;
-    }
-    const getAndJoinGenres = () => {
-        const genresList = [];
+    //     return hours >=1 ? `${hours}h ${minutes}m` : `${minutes}m`;
+    // }
+    // const getAndJoinGenres = () => {
+    //     const genresList = [];
 
-        movie.genres.forEach(genre => {
-            genresList.push(genre.name);
-        });
+    //     movie.genres.forEach(genre => {
+    //         genresList.push(genre.name);
+    //     });
 
-        return genresList.join(', ');
-    }
+    //     return genresList.join(', ');
+    // }
 
     const getCertification = () => {
-        const apiPath = releaseDate[0]['release_dates'];
-        const findCertification = apiPath.find(({ certification }) => certification !== '');
-        
-        // Temp Fix
-        if (findCertification === undefined) {
-            return '404';
-        }
-
+        const releaseDates = media['release_dates']['results'];
+        const findUSReleaseDates = releaseDates.find(({ iso_3166_1 }) => iso_3166_1 === 'US');
+        const findCertification = findUSReleaseDates['release_dates'].find(({ certification }) => certification !== '');
         const certification = findCertification['certification'];
 
-        return certification;
+        if (findCertification === undefined) return '404';
+
+        return certification
     }
 
     const getDate = (format) => {
-        const apiDate = new Date(releaseDate[0]['release_dates'][0]['release_date']);
+        // const apiDate = new Date(media.release_date[0]['release_dates'][0]['release_date']);
+        const apiDate = new Date(media.release_date || media.first_air_date);
         const year = apiDate.getUTCFullYear();
         const month = apiDate.getUTCMonth() + 1;
         const day = apiDate.getUTCDate();
@@ -49,22 +47,25 @@ const OverviewTitle = ({ movie, releaseDate }) => {
         return null;
     }
 
-    return (
-        <div className='overviewTitle'>            
-            <div className='overview_Title'>
-                <h1>{title}</h1>
-                <h1 className='title_Date'>{`(${getDate('yyyy')})`}</h1>
+    if (format === 'movie') {
+        return (
+            <div className='overviewTitle'>            
+                <div className='overview_Title'>
+                    <h1>{title}</h1>
+                    <h1 className='title_Date'>{`(${getDate('yyyy')})`}</h1>
+                </div>
+                <div className='overviewFacts'> 
+                    <p className='overviewCert'>{getCertification()}</p>
+                    {/* <p>{getDate('mm/dd/yyyy')}</p>
+                    <p className='bulletPoint'>•</p>
+                    <p>{getAndJoinGenres()}</p>
+                    <p className='bulletPoint'>•</p>
+                   <p>{convertRuntime()}</p> */}
+                </div>
             </div>
-            <div className='overviewFacts'> 
-                <p className='overviewCert'>{getCertification()}</p>
-                <p>{getDate('mm/dd/yyyy')}</p>
-                <p className='bulletPoint'>•</p>
-                <p>{getAndJoinGenres()}</p>
-                <p className='bulletPoint'>•</p>
-               <p>{convertRuntime()}</p>
-            </div>
-        </div>
-    )
+        );
+    }
+    
 };
 
 export default OverviewTitle;
