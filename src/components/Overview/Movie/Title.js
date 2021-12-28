017c6042ab35eadf0d62c2c4bdf513f45b0018b8
robisonwebdev <david@robisonweb.dev>
2021-12-28 10:15:37 -0700

    Setup getDate component to find movie date and return it.

--- src/components/Overview/Movie/Title.js
+++ src/components/Overview/Movie/Title.js
@@ -2,11 +2,27 @@ import React from 'react';
 import '../../../styles/Overview/Title.css';
 
 const Title = ({ movie }) => {
+    const getDate = (format) => {
+        const releaseDates = movie['release_dates']['results'];
+        const findUSReleaseDates = releaseDates.find(({ iso_3166_1 }) => iso_3166_1 === 'US');
+        const findReleaseDate = findUSReleaseDates['release_dates'].find(({ release_date }) => release_date !== '');
+        const getDate = new Date(findReleaseDate['release_date']);
+        const year = getDate.getUTCFullYear();
+        const month = getDate.getUTCMonth() + 1;
+        const day = getDate.getUTCDate();
+
+        if (format === 'yyyy') return year;
+
+        if (format === 'mm/dd/yyyy') return `${month}/${day}/${year} (US)`;
+
+        return null;
+    };
+    
     return (
         <div className='overview_title'>            
             <div className='overview_title_bar'>
                 <h1>{movie.title}</h1>
-                {/* <h1 className='title_Date'>{`(${getMovieDate('yyyy')})`}</h1> */}
+                <h1 className='title_date'>{`(${getDate('yyyy')})`}</h1>
             </div>
             <div className='overview_title_facts'> 
                 {/* <p className='overviewCert'>{getMovieCertification()}</p> */}
