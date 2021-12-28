a3eae9b7f4d7e8031b58d0c816611ec4efb6bd4e
robisonwebdev <david@robisonweb.dev>
2021-12-28 10:09:19 -0700

    Copied code from Title.js in Overview folder and moved it into file. Commented out code that not setup. Changed classNames.

--- src/components/Overview/Movie/Title.js
+++ src/components/Overview/Movie/Title.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../../../styles/Overview/Title.css';
 
 const Title = () => {
     return (
-        <div>
-            
+        <div className='overview_title'>            
+            <div className='overview_title_bar'>
+                {/* <h1>{title}</h1> */}
+                {/* <h1 className='title_Date'>{`(${getMovieDate('yyyy')})`}</h1> */}
+            </div>
+            <div className='overview_title_facts'> 
+                {/* <p className='overviewCert'>{getMovieCertification()}</p> */}
+                {/* <p>{getMovieDate('mm/dd/yyyy')}</p> */}
+                {/* <p className='bulletPoint'>•</p> */}
+                {/* <p>{getAndJoinGenres()}</p> */}
+                {/* <p className='bulletPoint'>•</p> */}
+                {/* <p>{convertRuntime()}</p> */}
+            </div>
         </div>
     )
 };
