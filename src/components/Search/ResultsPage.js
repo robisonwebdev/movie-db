4915612b1a742cde6c114d7da6d68ed86fa71815
robisonwebdev <david@robisonweb.dev>
2021-12-29 16:32:24 -0700

    Committing to load on another computer.

--- src/components/Search/ResultsPage.js
+++ src/components/Search/ResultsPage.js
@@ -1,8 +1,9 @@
-import React, { useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useRef, useState} from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
 import Filter from './Filter';
 import Search from './Search';
+import Pagination from '../Pagination';
 import '../../styles/Search/ResultsPage.css';
 import Results from './Results';
 
@@ -11,6 +12,7 @@ const ResultsPage = () => {
     const [apiKey] = useState('9289aca3a6413b200619b263ac82e4c0');
     const [collections, setCollections] = useState([]);
     const [companies, setComponies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1)
     const [keywords, setKeywords] = useState([]);
     const [filters, setFilters] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -20,12 +22,15 @@ const ResultsPage = () => {
     const [shows, setShows] = useState([]);
     const [mediaList, setMediaList] = useState([]);
     const [mediaType, setMediaType] = useState('')
+    const initialRender = useRef(true);
 
     const handleFilterSelection = (filter) => {
         filters.forEach(object => {
             if (filter === object.filter) {
+                setCurrentPage(1);
                 setMediaList(object.filter);
                 setMediaType(object.type);
+                
             }
         })
     };
@@ -72,66 +77,149 @@ const ResultsPage = () => {
         }))
     }, [apiKey]);
 
+    const fetchPageData = useCallback((page) => {
+        let update_API;
+
+        if (mediaType === 'shows') {
+            update_API = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&language=en-US&page=${page}&query=${searchValue}&include_adult=false`;
+        }
+
+        if (mediaType === 'movie') {
+            update_API = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchValue}&page=${page}&include_adult=false`;
+        }
+
+        if (mediaType === 'people') {
+            update_API = `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&language=en-US&query=${searchValue}&page=${page}&include_adult=false`;
+        }
+
+        if (mediaType === 'keywords') {
+            update_API = `https://api.themoviedb.org/3/search/keyword?api_key=${apiKey}&query=${searchValue}&page=${page}`;
+        }
+
+        if (mediaType === 'collection') {
+            update_API = `https://api.themoviedb.org/3/search/collection?api_key=${apiKey}&language=en-US&query=${searchValue}&page=${page}`;
+        }
+
+        if (mediaType === 'companies') {
+            update_API = `https://api.themoviedb.org/3/search/company?api_key=${apiKey}&query=${searchValue}&page=${page}`;
+        }
+
+        // setLoading(true);
+
+        axios
+        .get(update_API)
+        .then(res => {
+            if (mediaType === 'shows') {
+                setShows(res.data);
+                setMediaList(shows);
+            }
+    
+            if (mediaType === 'movie') {
+                setMovies(res.data);
+                setMediaList(movies);
+            }
+    
+            if (mediaType === 'people') {
+                setPeople(res.data);
+                setMediaList(people);
+            }
+    
+            if (mediaType === 'keywords') {
+                setKeywords(res.data);
+                setMediaList(keywords);
+            }
+    
+            if (mediaType === 'collection') {
+                setCollections(res.data);
+                setMediaList(collections);
+            }
+    
+            if (mediaType === 'companies') {
+                setComponies(res.data);
+                setMediaList(companies);
+            }
+
+            // setLoading(false);
+        })
+        .catch(err => console.log(err))
+    }, [apiKey, searchValue, mediaType, setMediaList]);
+
+    // Runs on initial render and searchValue change.
     useEffect(() => {
         fetchData(searchValue);
-    }, [fetchData, searchValue])
+    }, [fetchData, searchValue]);
+
+    // No Initial Render. Runs on currentPage Change.
+    useEffect(() => {
+        if (initialRender.current) {
+            initialRender.current = false;
+        } else {
+            fetchPageData(currentPage)
+        }
+    }, [fetchPageData, currentPage])
+
+    const testFun = () => {
+        const filtersArray = [
+            {
+                filter: shows,
+                id: 'shows_001',
+                name: 'TV Shows',
+                type: 'shows'
+            },
+            {
+                filter: movies,
+                id: 'movies_001',
+                name: 'Movies',
+                type: 'movie'
+            },
+            {
+                filter: people,
+                id: 'people_001',
+                name: 'People',
+                type: 'people'
+            },
+            {
+                filter: keywords,
+                id: 'keywords_001',
+                name: 'Keywords',
+                type: 'keywords'
+            },
+            {
+                filter: collections,
+                id: 'collections_001', 
+                name: 'Collections',
+                type: 'collection'
+            },
+            {
+                filter: companies,
+                id: 'companies_001',
+                name: 'Companies',
+                type: 'companies'
+            }
+        ];
+
+        filtersArray.sort((a, b) => {
+            return b.filter.total_results - a.filter.total_results;
+        });
+
+        setFilters(filtersArray);
+        setMediaList(filtersArray[0].filter);
+        setMediaType(filtersArray[0].type)
+        console.log(`Collection`, collections);
+        // console.log(`Companies`, companies);
+        // console.log(`Keywords`, keywords);
+        console.log(`Movies`, movies);
+        // console.log(`People`, people);
+        // console.log(`Shows`, shows);
+    }
 
     useEffect(() => {
         if (!loading) {
-            const filtersArray = [
-                {
-                    filter: shows,
-                    id: 'shows_001',
-                    name: 'TV Shows',
-                    type: 'shows'
-                },
-                {
-                    filter: movies,
-                    id: 'movies_001',
-                    name: 'Movies',
-                    type: 'movie'
-                },
-                {
-                    filter: people,
-                    id: 'people_001',
-                    name: 'People',
-                    type: 'people'
-                },
-                {
-                    filter: keywords,
-                    id: 'keywords_001',
-                    name: 'Keywords',
-                    type: 'keywords'
-                },
-                {
-                    filter: collections,
-                    id: 'collections_001', 
-                    name: 'Collections',
-                    type: 'collection'
-                },
-                {
-                    filter: companies,
-                    id: 'companies_001',
-                    name: 'Companies',
-                    type: 'companies'
-                }
-            ];
-
-            filtersArray.sort((a, b) => {
-                return b.filter.total_results - a.filter.total_results;
-            });
-
-            setFilters(filtersArray);
-            setMediaList(filtersArray[0].filter);
-            setMediaType(filtersArray[0].type)
-            // console.log(`Collection`, collections);
-            console.log(`Companies`, companies);
-            // console.log(`Keywords`, keywords);
-            // console.log(`Movies`, movies);
-            // console.log(`People`, people);
-            // console.log(`Shows`, shows);
+            testFun();
         }
-    }, [loading, collections, companies, keywords, movies, people, shows]);
+    }, [loading]);
+
+    console.log(mediaList)
 
     return (
         <section className='resultsPage'>
@@ -145,7 +233,10 @@ const ResultsPage = () => {
                         {buildFiltersList}
                     </div>
                 </div>
-                <Results media={mediaList} type={mediaType} />
+                <div className='results'>
+                    <Results media={mediaList} type={mediaType} />
+                    <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={mediaList.total_pages} />
+                </div>
             </div>
         </section>
     )
