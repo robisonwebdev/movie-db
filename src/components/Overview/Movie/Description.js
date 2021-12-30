07dd69fd55acb81b46f20731fe36862e61f97bf8
robisonwebdev <david@robisonweb.dev>
2021-12-30 10:10:12 -0700

    Destrucutred movie prop. Inside return div added p(set to movie.tagline) with className of tagline, h2 with Overview text, and p (set to movie.overview).

--- src/components/Overview/Movie/Description.js
+++ src/components/Overview/Movie/Description.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Description = () => {
+const Description = ({ movie }) => {
     return (
         <div className='overview_description'>
-            
+            <p className='tagline'>{movie.tagline}</p>
+            <h2>Overview</h2>
+            <p>{movie.overview}</p>
         </div>
     )
 };
