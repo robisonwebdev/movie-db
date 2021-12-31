3a71a95b854323081432746535a6e34bac3ff29a
robisonwebdev <david@robisonweb.dev>
2021-12-31 16:10:39 -0700

    Imported useEffect. Setup state for director, producer, and screenplay. Called useEffect to get director, producer, and screenplay on load and changes to crew state. Setup 2nd component called CrewInfo to display name and job of crew. Called CrewInfo in overview_crew div.

--- src/components/Overview/Movie/Crew.js
+++ src/components/Overview/Movie/Crew.js
@@ -1,13 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Crew = ({ credits }) => {
     const [crew] = useState(credits.crew);
+    const [director, setDirector] = useState('');
+    const [producer, setProducer] = useState('');
+    const [screenplay, setScreenplay] = useState('');
+
+    useEffect(() => {
+        crew.find(member => {
+            // Get Director
+            if (member.job === 'Director') {
+                setDirector(member.name);
+            }
+
+            // Get Screenplay
+            if (member.job === 'Screenplay') {
+                setScreenplay(member.name);
+            }
+
+            // Get Producer
+            if (member.job === 'Producer') {
+                setProducer(member.name);
+            }
+
+            return null;
+        });
+    }, [crew]);
 
     return (
         <div className='overview_crew'>
-            
+            <CrewInfo job={'Director'} name={director} />            
+            <CrewInfo job={'Producer'} name={producer} />            
+            <CrewInfo job={'Screenplay'} name={screenplay} />
         </div>
     )
 };
+
+const CrewInfo = ({ job, name }) => {
+    return (
+        <div className='crew'>
+            <h4>{name}</h4>
+            <p>{job}</p>
+        </div>
+    );
+}
 
 export default Crew;
