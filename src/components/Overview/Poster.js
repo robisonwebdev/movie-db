57edc9b08d2ca57e4a71db83593750dd0bdd3f9c
robisonwebdev <david@robisonweb.dev>
2021-12-31 16:16:22 -0700

    Changed poster address path.

--- src/components/Overview/Poster.js
+++ src/components/Overview/Poster.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../styles/Overview/Poster.css';
 
 const Poster = ({ media }) => {
-    const poster = `https://image.tmdb.org/t/p/w300/${media.poster_path}`;
+    const poster = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${media.poster_path}`;
 
     return (
         <img className='overview_poster' src={poster} alt={media.title} />
