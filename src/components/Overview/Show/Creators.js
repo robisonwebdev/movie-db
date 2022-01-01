013556af06b537a96bc9503d269bd577d209df5b
robisonwebdev <david@robisonweb.dev>
2022-01-01 14:47:04 -0700

    Setup Creators component to display the creators of inputted show.

--- src/components/Overview/Show/Creators.js
+++ src/components/Overview/Show/Creators.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../../../styles/Overview/Crew.css';
 
-const Creators = () => {
+const Creators = ({ show }) => {
+    const creators = show.created_by?.map(creator => {
+        return (
+            <div className='creator' key={creator.id}>
+                <h4>{creator.name}</h4>
+                <p>Creator</p>
+            </div>
+        );
+    })
+
     return (
         <div className='overview_creators'>
-            
+            {creators}
         </div>
     )
 };
