96506ae2d3517b29329008858d6bace62c08604d
robisonwebdev <david@robisonweb.dev>
2022-01-01 16:33:33 -0700

    Setup component to get collection title and list of genres, and return them.

--- src/components/Overview/Collection/Title.js
+++ src/components/Overview/Collection/Title.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import api_key from '../../../data/Key';
 
-const Title = () => {
-    return (
-        <div>
+const Title = ({ collection }) => {
+    const [loading, setLoading] = useState(true);
+    const [movieCodes, setMovieCodes] = useState([]);
+    const [showCodes, setShowCodes] = useState([]);
+
+    const fetchData = useCallback(() => {
+        const movieAPI = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}&language=en-US`;
+        const showAPI = `https://api.themoviedb.org/3/genre/tv/list?api_key=${api_key}&language=en-US`;
+
+        const getMovieData = axios.get(movieAPI);
+        const getShowData = axios.get(showAPI);
+
+        setLoading(true);
+
+        axios
+        .all([getMovieData, getShowData])
+        .then(axios.spread((...allData) => {
+            const movieData = allData[0].data.genres;
+            const showData = allData[1].data.genres;
+
+            setMovieCodes(movieData);
+            setShowCodes(showData);
+            setLoading(false);
+        }))
+        .catch(err => console.log(err))
+    }, [])
+
+    const getAndJoinGenres = () => {
+        const genresIDs = [];
+        const genres = [];
+
+        // Build array of genres
+        collection.parts.forEach(part => {
+            const gernes = part.genre_ids;
             
+            gernes.forEach(genre => {
+                if (genresIDs.includes(genre) !== true) {
+                    genresIDs.push(genre);
+                }
+            })
+        });
+
+        // Find name for each genre code
+        genresIDs.forEach(genre => {
+            movieCodes.find(code => {
+                if (code.id === genre) {
+                    genres.push(code.name);
+                } else {
+                    showCodes.find(code => {
+                        if (code.id === genre) {
+                            genres.push(code.name);
+                        }
+
+                        return null;
+                    });
+                };
+
+                return null;
+            });
+        });
+
+        return genres.join(', ');
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData])
+
+    return (
+        <div className='overview_title'>            
+            <div className='overview_title_bar'>
+                <h1>{collection.name}</h1>
+            </div>
+            <div className='overview_title_facts'> 
+                {loading ? null : <p>{getAndJoinGenres()}</p>}
+            </div>
         </div>
     )
 };
