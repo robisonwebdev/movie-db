0d3970971fd44880fa638c53e3751268295685b7
robisonwebdev <david@robisonweb.dev>
2022-01-01 14:59:12 -0700

    Imported useCallback, useEffect, useState, useParams, axios, and api_key. Setup state for collection, loading. Setup id to useParams. Setup fetchData function to get collection data and set it to collection state. Setup useEffect to call fetchData.

--- src/components/Overview/Collection/Collection.js
+++ src/components/Overview/Collection/Collection.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import api_key from '../../../data/Key';
 
 const Collection = () => {
+    const [collection, setCollection] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+
+    const fetchData = useCallback(() => {
+        const collectionAPI = `https://api.themoviedb.org/3/collection/${id}?api_key=${api_key}&language=en-US`;
+
+        setLoading(true);
+
+        axios
+        .get(collectionAPI)
+        .then(res => {
+            setCollection(res.data)
+            setLoading(false)
+        })
+        .catch(err => console.log(err))
+    }, [id])
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
     return (
-        <div className='collection_Overview'>
-            
+        <div className='collection_overview'>
+            {loading ? null : console.log(collection)}
         </div>
     )
 };
