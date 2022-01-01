e85add22bb4a1e60db40e17747b688050e431892
robisonwebdev <david@robisonweb.dev>
2022-01-01 14:48:46 -0700

    Added loading state. Moved Header component into a ternery set to loading.

--- src/components/Overview/Show/Show.js
+++ src/components/Overview/Show/Show.js
@@ -2,19 +2,23 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import api_key from '../../../data/Key'
-import Header from '../Header';
+import Header from './Header';
 
 const Show = () => {
+    const [loading, setLoading] = useState(true);
     const [showData, setShowData] = useState([]);
     const { id } = useParams();
 
     const fetchData = useCallback(() => {
         const showAPI = `https://api.themoviedb.org/3/tv/${id}?api_key=${api_key}&append_to_response=first_air_date,credits,content_ratings&language=en-US`;
 
+        setLoading(true);
+
         axios
         .get(showAPI)
         .then(res => {
             setShowData(res.data);
+            setLoading(false);
         })
         .catch(err => console.log(err))
     }, [id]);
@@ -25,7 +29,7 @@ const Show = () => {
 
     return (
         <div className='overview'>
-            <Header media={showData} format='show' />
+            {loading ? null : <Header show={showData} />}
             {console.log('showData', showData)}
         </div>
     )
