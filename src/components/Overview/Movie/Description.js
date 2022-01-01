d6c88df89474bc0a2a5f0ef98a4aa0b4380f0ebf
robisonwebdev <david@robisonweb.dev>
2022-01-01 12:53:02 -0700

    Setup ternery to look for tagline and return null if true or p tag.

--- src/components/Overview/Movie/Description.js
+++ src/components/Overview/Movie/Description.js
@@ -4,7 +4,7 @@ import '../../../styles/Overview/Description.css';
 const Description = ({ movie }) => {
     return (
         <div className='overview_description'>
-            <p className='tagline'>{movie.tagline}</p>
+            {movie.tagline === "" ? null : <p className='tagline'>{movie.tagline}</p>}
             <h2>Overview</h2>
             <p>{movie.overview}</p>
         </div>
