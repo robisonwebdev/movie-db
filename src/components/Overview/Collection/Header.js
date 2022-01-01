dbc68450b6a11fbac7cb22d87c252cea4c80e263
robisonwebdev <david@robisonweb.dev>
2022-01-01 15:03:29 -0700

    Imported Header.css Setup variable to get backdrop image and varible to set styling for background. Gave return div a className of overview_header and set style to backdropStyle.

--- src/components/Overview/Collection/Header.js
+++ src/components/Overview/Collection/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../../../styles/Overview/Header.css';
 
-const Header = () => {
+const Header = ({ collection }) => {
+    const backdropImage = `http://image.tmdb.org/t/p/original/${collection.backdrop_path}`;
+    const backdropStyle = {
+        background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backdropImage})`,
+        backgroundPosition: 'center top',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    };
+
     return (
-        <div>
+        <div className='overview_header' style={backdropStyle}>
             
         </div>
     )
