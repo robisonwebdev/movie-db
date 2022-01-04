e12e562540707b80870d56bf9b0511b7e3b69608
robisonwebdev <david@robisonweb.dev>
2022-01-04 15:45:25 -0700

    Setup component to get data on languages. Displays movie status, original language, budget, and revenue.

--- src/components/Overview/Movie/Facts.js
+++ src/components/Overview/Movie/Facts.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import api_key from '../../../data/Key';
 
-const Facts = () => {
+const Facts = ({ movie }) => {
+    const [languages, setLanguages] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { budget, original_language, revenue, status } = movie;
+
+    const fetchData = useCallback(() => {
+        const languagesAPI = ` https://api.themoviedb.org/3/configuration/languages?api_key=${api_key}`;
+
+        setLoading(true);
+
+        axios
+        .get(languagesAPI)
+        .then(res => {
+            setLanguages(res.data);
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData])
+
+    const getLanguageString = () => {  
+        let language = '';
+
+        languages.forEach(obj => {
+            if (obj['iso_639_1'] === original_language) {
+                language = obj.english_name;
+            }
+        })
+
+        return language;
+    }
+
     return (
-        <div>
-            
-        </div>
+        <section className='overview_facts'>
+            <div>
+                <strong>Status</strong>
+                <p>{status}</p>
+            </div>
+            <div>
+                <strong>Original Language</strong>
+                <p>{loading ? null : getLanguageString()}</p>
+            </div>
+            <div>
+                <strong>Budget</strong>
+                <p>{`$${budget.toLocaleString('en-US')}`}</p>
+            </div>
+            <div>
+                <strong>Revenue</strong>
+                <p>{`$${revenue.toLocaleString('en-US')}`}</p>
+            </div>
+        </section>
     )
 };
 
