bf466bdae044f10641ec513c7aac13a8a03762e0
robisonwebdev <david@robisonweb.dev>
2022-01-04 15:18:48 -0700

    Setup component to display top nine cast.

--- src/components/Overview/Movie/TopCast.js
+++ src/components/Overview/Movie/TopCast.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import CastCard from './CastCard';
+import '../../../styles/Overview/TopBilledCast.css';
 
-const TopCast = () => {
+const TopCast = ({ cast }) => {
+    const topCast = cast.slice(0, 9);
+
+    const topNine = topCast.map(person => {
+        return <CastCard key={person.id} person={person} />
+    })
+
     return (
-        <div>
-            
+        <div className='overview_topCast'>
+            <h3>Top Billed Cast</h3>
+            <div className='topCast'>
+                {topNine}
+                <h4 className='viewMore'>View More <i className='las la-arrow-right' /></h4>
+            </div>
         </div>
     )
 };
