1476a9df17661b327be6b4cd664c11b9eb143d14
robisonwebdev <david@robisonweb.dev>
2022-01-04 15:20:39 -0700

    Setup component to display a persons picture, name, and character name. Also to link to persons page.

--- src/components/Overview/Movie/CastCard.js
+++ src/components/Overview/Movie/CastCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../../styles/Overview/CastCard.css';
 
-const CastCard = () => {
+const CastCard = ({ person }) => {
+    const { character, id, name, profile_path } = person;
+    const poster = `https://image.tmdb.org/t/p/w185/${profile_path}`;
+
     return (
-        <div>
-            
+        <div className='castCard'>
+            <Link to={`/person/${id}`}><img src={poster} alt={name} /></Link>
+            <div className='castCard_Info'>
+                <Link to={`person/${id}`}><h4>{name}</h4></Link>
+                <p>{character}</p>
+            </div>
         </div>
     )
 };
