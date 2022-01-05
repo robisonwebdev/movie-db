49a318d191f8daf4ebcce7c9e9056006d0b5736e
robisonwebdev <david@robisonweb.dev>
2022-01-05 13:25:34 -0700

    Imported Keywords.js Added Keywords component below Facts component and passed it keywords prop.

--- src/components/Overview/Movie/Stats.js
+++ src/components/Overview/Movie/Stats.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Facts from './Facts';
+import Keywords from './Keywords';
 import '../../../styles/Overview/Stats.css';
 
 const Stats = ({ movie }) => {
     return (
         <div className='overview_stats'>
             <Facts movie={movie} />
+            <Keywords keywords={movie.keywords.keywords} />
         </div>
     )
 };
