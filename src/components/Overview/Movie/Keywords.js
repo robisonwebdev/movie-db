d36f86003868d5e98f2740fe8d018a220baaf8bd
robisonwebdev <david@robisonweb.dev>
2022-01-05 13:02:27 -0700

    Setup component to get keywords from keywords prop and display them in an list.

--- src/components/Overview/Movie/Keywords.js
+++ src/components/Overview/Movie/Keywords.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const Keywords = () => {
+const Keywords = ({ keywords }) => {
+    const getKeywords = keywords.map(keyword => {
+        return (
+            <li key={keyword.id}>{keyword.name}</li>
+        );
+    })
+
     return (
-        <div>
-            
-        </div>
+        <section className='overview_keywords'>
+            <h4>Keywords</h4>
+            <div className='keywords'>
+                <ul>
+                    {getKeywords}
+                </ul>
+            </div>
+        </section>
     )
 };
 
