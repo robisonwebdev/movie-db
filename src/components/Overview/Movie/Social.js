9feec483f11f085cd487ffa5c6e4579ee632c099
robisonwebdev <david@robisonweb.dev>
2022-01-06 16:37:37 -0700

    Destrcutred movie. Setup variable to get number of reviews. Gave section a className of overview_social and borderBottom. Inside return section, set two divs with classNames of social_header and social_content. Inside social_header, set h2 with Social title and ul.

--- src/components/Overview/Movie/Social.js
+++ src/components/Overview/Movie/Social.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../../../styles/Overview/Social.css';
 
-const Social = () => {
+const Social = ({ movie }) => {
+    const reviewsTotal = movie.reviews.total_results;
+
     return (
-        <section>
-            
+        <section className='overview_social borderBottom'>
+            <div className='social_header'>
+                <h2>Social</h2>
+                <ul>
+                    <li>Reviews <span>{reviewsTotal}</span></li>
+                </ul>
+            </div>
+            <div className='social_content'>
+                TEMP
+            </div>
         </section>
     )
 };
