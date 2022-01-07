b3550020c5877def345577dd82c289e653c3a29e
robisonwebdev <david@robisonweb.dev>
2022-01-07 14:53:35 -0700

    Imported Review.css Setup getAvatar function to get avatar_path from api and return it. Setup getRating function to get rating from api and return it. Setup getWrittenDate function to get the date of when review was written and return it. Setup the initial return structure.

--- src/components/Overview/Movie/ReviewCard.js
+++ src/components/Overview/Movie/ReviewCard.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import '../../../styles/Overview/Review.css';
 
-const ReviewCard = () => {
+const ReviewCard = ({ review }) => {
+    const getUsername = review.author_details.username;
+
+    const getAvatar = () => {
+        const avatarPath = review.author_details.avatar_path;
+
+        if (avatarPath.includes('http')) {
+            return avatarPath.slice(1);
+        }
+
+        return `https://image.tmdb.org/t/p/w64_and_h64_face/${avatarPath}`;
+    }
+
+    const getRating = () => {
+        if (review.author_details.rating !== null) {
+            const rating = review.author_details.rating.toFixed(1);
+            return (
+                <p className='review_rating'><i className='las la-star' /> {rating}</p>
+            );
+        }
+
+        return null;
+    }
+
+    const getWrittenDate = () => {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const initalDate = review.created_at;
+        const newDate = new Date(initalDate);
+        const month = newDate.getUTCMonth();
+        const day = newDate.getUTCDate();
+        const year = newDate.getUTCFullYear();
+
+        return `${months[month]} ${day}, ${year}`;
+    }
+
     return (
-        <section>
-            
+        <section className='review_card'>
+            <div className='review_header'>
+                <img src={getAvatar()} alt={`${getUsername} avatar`} />
+                <div className='review_title'>
+                    <div>
+                        <h3>{`A review by ${getUsername}`}</h3>
+                        {getRating()}
+                    </div>
+                    <p className='review_writtenBy'>{`Written by ${getUsername.toUpperCase()} on ${getWrittenDate()}`}</p>
+                </div>
+            </div>
+            <div className='review_content'>
+                {console.log(review)}
+                <p>{review.content}</p>
+            </div>
         </section>
     )
 };
