05f00dbf5306f5737508e3a994bdc92cbcbdab58
robisonwebdev <david@robisonweb.dev>
2022-01-08 12:22:22 -0700

    Added videos to append_to_response inside on fetchData.

--- src/components/Overview/Movie/Movie.js
+++ src/components/Overview/Movie/Movie.js
@@ -15,7 +15,7 @@ const Movie = () => {
     const { id } = useParams();
 
     const fetchData = useCallback(() => {
-        const movieAPI = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&append_to_response=release_dates,credits,keywords,recommendations,reviews&&language=en-US`;
+        const movieAPI = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&append_to_response=release_dates,credits,keywords,recommendations,reviews,videos&&language=en-US`;
 
         setLoading(true);
 
