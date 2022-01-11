27426c3ee973e851829e3f6efdc3d6d6fc33fbc7
robisonwebdev <david@robisonweb.dev>
2022-01-11 14:31:39 -0700

    Initial componet setup.

--- src/components/Overview/Show/SeriesCast.js
+++ src/components/Overview/Show/SeriesCast.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import CastCard from './CastCard';
+import '../../../styles/Overview/CastAndCrew.css';
 
-const SeriesCast = () => {
+const SeriesCast = ({ cast }) => {
+    const topCast = cast.slice(0, 9);
+
+    const topNine = topCast.map(person => {
+        return <CastCard key={person.id} person={person} />
+    });
+
     return (
-        <section>
-            
+        <section className='overview_seriesCast borderBottom'>
+            <h2>Series Cast</h2>
+            <div className='seriesCast'>
+                {topNine}
+                <h4 className='viewMore'>View More <i className='las la-arrow-right' /></h4>
+            </div>
+            <h3>{`Full Cast & Crew`}</h3>
         </section>
     )
 };
