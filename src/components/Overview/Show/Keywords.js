0f17a4d513010a9d501a9e0eaa338e2029068f1c
robisonwebdev <david@robisonweb.dev>
2022-01-13 08:25:53 -0700

    Imported Keywords.css Destructured keywords prop. Setup variable of getKeywords to map over keywords prop and return <li>. Gave return section a className of overview_keywords. Inside section added h3 and div with className of keywords. Inside .keywords div, added ul and called getKeywords.

--- src/components/Overview/Show/Keywords.js
+++ src/components/Overview/Show/Keywords.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../../../styles/Overview/Keywords.css';
 
-const Keywords = () => {
+const Keywords = ({ keywords }) => {
+    const getKeywords = keywords.map(keyword => {
+        return (
+            <li key={keyword.id} className='keyword'>{keyword.name}</li>
+        );
+    });
+
     return (
-        <section>
-            
+        <section className='overview_keywords'>
+            <h3>Keywords</h3>
+            <div className='keywords'>
+                <ul>
+                    {getKeywords}
+                </ul>
+            </div>
         </section>
     )
 };
