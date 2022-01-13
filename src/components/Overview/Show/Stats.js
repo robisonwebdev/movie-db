e7d114dead3416e6ca734b3c29e4f4606623eb99
robisonwebdev <david@robisonweb.dev>
2022-01-13 08:26:26 -0700

    Imported Keywords component and added it to return section.

--- src/components/Overview/Show/Stats.js
+++ src/components/Overview/Show/Stats.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Facts from './Facts';
+import Keywords from './Keywords';
 
-const Stats = () => {
+const Stats = ({ show }) => {
     return (
-        <section>
-            
+        <section className='overview_stats borderBottom'>
+            <Facts show={show} />
+            <Keywords keywords={show.keywords.results} />
         </section>
     )
 };
