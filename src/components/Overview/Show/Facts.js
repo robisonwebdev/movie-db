ef1363c518a8b25279916806a0b10870acb8661c
robisonwebdev <david@robisonweb.dev>
2022-01-12 18:40:26 -0700

    Added if statement in getNetwork to account for no network logo. Added 3rd div in .overview_facts to display type.

--- src/components/Overview/Show/Facts.js
+++ src/components/Overview/Show/Facts.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../../../styles/Overview/Facts.css';
 
 const Facts = ({ show }) => {
-    const { networks, status } = show;
+    const { networks, status, type } = show;
 
     const getNetwork = () => {
-        const networkLogo = `http://image.tmdb.org/t/p/h30/${networks[0].logo_path}`
+        const networkLogo = `http://image.tmdb.org/t/p/h30/${networks[0]?.logo_path}`;
+
+        if (networks.length === 0) return '-';
 
         return <img src={networkLogo} alt={networks.name} />
-    }
+    };
 
     return (
         <section className='overview_facts'>
@@ -20,6 +22,10 @@ const Facts = ({ show }) => {
             <div>
                 <strong>Network</strong>
                 {getNetwork()}
+            </div>
+            <div>
+                <strong>Type</strong>
+                <p>{type}</p>
             </div>
         </section>
     )
