b325945d81f5bc3e1e13760af42fb522b7444149
robisonwebdev <david@robisonweb.dev>
2022-01-14 11:17:40 -0700

    Changed .last_season to .last_season_card Setup functions getEpisodes, getImage, and getYear. Setup last_season_card.

--- src/components/Overview/Show/LastSeason.js
+++ src/components/Overview/Show/LastSeason.js
@@ -4,11 +4,46 @@ const LastSeason = ({ seasons }) => {
     const seasonsWithAirDates = seasons.filter(season => season.air_date !== null);
     const lastSeason = seasonsWithAirDates.pop();
 
+    const getEpisodes = () => {
+        if (lastSeason.episode_count === null) {
+            return 'No Episodes';
+        };
+
+        return `${lastSeason.episode_count} Episodes`;
+    };
+
+    const getImage = () => {
+        const imagePath = `https://image.tmdb.org/t/p/w130_and_h195_bestv2/${lastSeason.poster_path}`;
+
+        if (lastSeason.poster_path === null) {
+            const noImage = `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg`;
+
+            return <img src={noImage} alt={lastSeason.name} />
+        }
+
+        return <img src={imagePath} alt={lastSeason.name}/>
+    };
+
+    const getYear = () => {
+        const airDate = lastSeason.air_date;
+        const date = new Date(airDate);
+        const year = date.getUTCFullYear();
+
+        return year;
+    };
+
     return (
         <section className='overview_lastSeason'>
             <h3>Last Season</h3>
-            <div className='last_season'>
-                
+            <div className='last_season_card'>
+                {getImage()}
+                <div className='card_content'>
+                    <div className='card_title'>
+                        <h3>{lastSeason.name}</h3>
+                        <strong>{`${getYear()} | ${getEpisodes()}`}</strong>
+                    </div>
+                    <p>{lastSeason.overview}</p>
+                </div>
             </div>
             <h4>View All Seasons</h4>
         </section>
