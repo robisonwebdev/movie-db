351845a2f56852c2a0b1a649e4abbdf84a21c688
robisonwebdev <david@robisonweb.dev>
2022-01-14 10:56:00 -0700

    Destrucutred seasons. Setup seasonsWithAirDates to filter seasons with air dates. Setup lastSeason to take seasonsWithAirDates and grab last array. Gave return section a className of overview_lastSeason. Inside section added h3, div with className of last_season, and h4.

--- src/components/Overview/Show/LastSeason.js
+++ src/components/Overview/Show/LastSeason.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const LastSeason = () => {
+const LastSeason = ({ seasons }) => {
+    const seasonsWithAirDates = seasons.filter(season => season.air_date !== null);
+    const lastSeason = seasonsWithAirDates.pop();
+
     return (
-        <section>
-            
+        <section className='overview_lastSeason'>
+            <h3>Last Season</h3>
+            <div className='last_season'>
+                
+            </div>
+            <h4>View All Seasons</h4>
         </section>
     )
 };
