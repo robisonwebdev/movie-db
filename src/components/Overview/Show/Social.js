3f4166680bea3f2e2272e01825794dd821378f0d
robisonwebdev <david@robisonweb.dev>
2022-01-15 14:05:05 -0700

    Imported ReviewCard.js Setup component to display single review of targeted show, with future link to view all reviews.

--- src/components/Overview/Show/Social.js
+++ src/components/Overview/Show/Social.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import ReviewCard from './ReviewCard';
 import '../../../styles/Overview/Social.css';
 
-const Social = () => {
+const Social = ({ show }) => {
+    const reviews = show.reviews.results;
+    const reviewsTotal = show.reviews.total_results;
+    const randomReview = reviews[Math.floor(Math.random() * reviews.length)];
+
     return (
-        <section>
-            
+        <section className='overview_social borderBottom'>
+            <div className='social_header'>
+                <h2>Social</h2>
+                <ul>
+                    <li>Reviews <span>{reviewsTotal}</span></li>
+                </ul>
+            </div>
+            <div className='social_content'>                
+                {reviews.length === 0 ? <p>{`We don't have any reviews for ${show.title}`}</p> : <ReviewCard review={randomReview} />}
+                {reviews.length === 0 ? null : <strong>Read All Reviews</strong>}
+            </div>
         </section>
     )
 };
