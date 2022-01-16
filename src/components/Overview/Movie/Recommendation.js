cbe8eed2f777cbbf7d4fa3ea9f62b9907096a2a0
robisonwebdev <david@robisonweb.dev>
2022-01-16 09:45:15 -0700

    Destrucutred recommendation prop. Setup getRecommendations variable to map over recommendations and return img, title, and percentage. Gave section a className of overview_recommendation. Inside section added h1 and div with a className of recommendation_list. Called getRecommendation inside on .recommdation_list

--- src/components/Overview/Movie/Recommendation.js
+++ src/components/Overview/Movie/Recommendation.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const Recommendation = () => {
+const Recommendations = ({ recommendations }) => {
+    const getRecommendations = recommendations.map(item => {
+        const getImage = () => {
+            const imagePath = `https://image.tmdb.org/t/p/w250_and_h141_face/${item.poster_path}`;
+
+            return <img src={imagePath} alt={item.title} />
+        };
+
+        const getPercentage = () => {
+            const vote_average = item.vote_average * 10;
+
+            return Math.round(vote_average)
+        }
+
+        return (
+            <>
+                {getImage()}
+                <div className='recommendation_item'>
+                    <p>{item.title}</p>
+                    <p>{`${getPercentage()}%`}</p>
+                </div>
+            </>
+        );
+    });
+
     return (
-        <section>
+        <section className='overview_recommendations'>
             <h1>Recommendations</h1>
+            <div className='recommendation_list'>
+                {getRecommendations}
+            </div>
         </section>
     )
 };
 
-export default Recommendation;
+export default Recommendations;
