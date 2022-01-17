00c7879b1e13ecb3039e246e863ff73dc3033b07
robisonwebdev <david@robisonweb.dev>
2022-01-17 08:41:03 -0700

    Inside .media_header, added h2 and div with className of .media_nav Inside .media_nav, added 4 strong tags with text. Inside 3 of 4 strong tags, added length of videos, backdrops, and posters.

--- src/components/Overview/Movie/Media.js
+++ src/components/Overview/Movie/Media.js
@@ -6,7 +6,15 @@ const Media = ({ movie }) => {
 
     return (
         <section className='overview_media borderBottom'>
-            <div className='media_header'></div>
+            <div className='media_header'>
+                <h2>Media</h2>
+                <div className='media_nav'>
+                    <strong>Most Popular</strong>
+                    <strong>Videos {videos.length}</strong>
+                    <strong>Backdrops {backdrops.length}</strong>
+                    <strong>Posters {posters.length}</strong>
+                </div>
+            </div>
             <div className='media_content'></div>
         </section>
     )
