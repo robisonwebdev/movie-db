8c8dc08e57670e7db2d00a22c384f24c98cf8803
robisonwebdev <david@robisonweb.dev>
2022-01-17 10:39:39 -0700

    Setup getPosters function to get the first 6 posters and map through them, returning a img inside a div. Changed strong tags inside of .media_nav to h4 tags. Called getPosters inside of media_content.

--- src/components/Overview/Movie/Media.js
+++ src/components/Overview/Movie/Media.js
@@ -5,18 +5,36 @@ const Media = ({ movie }) => {
     const { backdrops, posters } = movie.images;
     const videos = movie.videos.results;
 
+    const getPosters = () => {
+        const firstSixPosters = posters.slice(0, 6);
+
+        const displayPosters = firstSixPosters.map(poster => {
+            const posterPath = `https://image.tmdb.org/t/p/w220_and_h330_face${poster.file_path}`;
+
+            return (
+                <div key={poster.file_path} className='media_poster'>
+                    <img src={posterPath} />
+                </div>
+            );
+        })
+
+        return displayPosters;
+    }
+
     return (
         <section className='overview_media borderBottom'>
             <div className='media_header'>
                 <h2>Media</h2>
                 <div className='media_nav'>
-                    <strong>Most Popular</strong>
-                    <strong>Videos {videos.length}</strong>
-                    <strong>Backdrops {backdrops.length}</strong>
-                    <strong>Posters {posters.length}</strong>
+                    <h4>Most Popular</h4>
+                    <h4>Videos <span>{videos.length}</span></h4>
+                    <h4>Backdrops <span>{backdrops.length}</span></h4>
+                    <h4>Posters <span>{posters.length}</span></h4>
                 </div>
             </div>
-            <div className='media_content'></div>
+            <div className='media_content'>
+                {getPosters()}
+            </div>
         </section>
     )
 };
