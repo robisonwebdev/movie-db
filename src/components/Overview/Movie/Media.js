34175be697dcc7d088a1bffba3add5cb5d5cb1b8
robisonwebdev <david@robisonweb.dev>
2022-01-18 10:28:22 -0700

    Setup state for viewAll, with an initial value of null. Added ternery to end of .media_nav that check viewAll for null value. Returns null it true, or p tag with className of media_view_all if true. Added setViewAll to all h4's onClick events in .media_nav

--- src/components/Overview/Movie/Media.js
+++ src/components/Overview/Movie/Media.js
@@ -3,6 +3,7 @@ import '../../../styles/Overview/Media.css';
 
 const Media = ({ movie }) => {
     const [mediaContent, setMediaContent] = useState();
+    const [viewAll, setViewAll] = useState(null);
     const { backdrops, posters } = movie.images;
     const videos = movie.videos.results;
 
@@ -57,11 +58,12 @@ const Media = ({ movie }) => {
             <div className='media_header'>
                 <h2>Media</h2>
                 <div className='media_nav'>
-                    <h4 onClick={() => setMediaContent()}>Most Popular</h4>
-                    <h4 onClick={() => setMediaContent()}>Videos <span>{videos.length}</span></h4>
-                    <h4 onClick={() => setMediaContent(getBackdrops())}>Backdrops <span>{backdrops.length}</span></h4>
-                    <h4 onClick={() => setMediaContent(getPosters())}>Posters <span>{posters.length}</span></h4>
+                    <h4 onClick={() => {setMediaContent(); setViewAll(null)}}>Most Popular</h4>
+                    <h4 onClick={() => {setMediaContent(); setViewAll('Videos')}}>Videos <span>{videos.length}</span></h4>
+                    <h4 onClick={() => {setMediaContent(getBackdrops()); setViewAll('Backdrops')}}>Backdrops <span>{backdrops.length}</span></h4>
+                    <h4 onClick={() => {setMediaContent(getPosters()); setViewAll('Posters')}}>Posters <span>{posters.length}</span></h4>
                 </div>
+                {viewAll === null ? null : <p className='media_view_all'>View All {viewAll}</p>}
             </div>
             <div className='media_main'>
                 {mediaContent}
