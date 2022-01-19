73e6866b2892d23b31b30f9383c7caf274450c57
robisonwebdev <david@robisonweb.dev>
2022-01-19 10:57:06 -0700

    Changed value getting passed to setMediaConent in useEffect to getVideos. Removed Most Popular h4 from .media_nav Removed ternery from .media_header's last element. Changed default value of viewAll to Videos.

--- src/components/Overview/Movie/Media.js
+++ src/components/Overview/Movie/Media.js
@@ -4,7 +4,7 @@ import '../../../styles/Overview/Media.css';
 
 const Media = ({ movie }) => {
     const [mediaContent, setMediaContent] = useState();
-    const [viewAll, setViewAll] = useState(null);
+    const [viewAll, setViewAll] = useState('Videos');    
     const { backdrops, posters } = movie.images;
     const videos = movie.videos.results;
 
@@ -66,20 +66,19 @@ const Media = ({ movie }) => {
     }, [videos]);
 
     useEffect(() => {
-        setMediaContent(getPosters());
-    }, [getPosters])
+        setMediaContent(getVideos());
+    }, [getVideos]);
 
     return (
         <section className='overview_media borderBottom'>
             <div className='media_header'>
                 <h2>Media</h2>
                 <div className='media_nav'>
-                    <h4 onClick={() => {setMediaContent(); setViewAll(null)}}>Most Popular</h4>
                     <h4 onClick={() => {setMediaContent(getVideos()); setViewAll('Videos')}}>Videos <span>{videos.length}</span></h4>
                     <h4 onClick={() => {setMediaContent(getBackdrops()); setViewAll('Backdrops')}}>Backdrops <span>{backdrops.length}</span></h4>
                     <h4 onClick={() => {setMediaContent(getPosters()); setViewAll('Posters')}}>Posters <span>{posters.length}</span></h4>
                 </div>
-                {viewAll === null ? null : <p className='media_view_all'>View All {viewAll}</p>}
+                <p className='media_view_all'>View All {viewAll}</p>
             </div>
             <div className='media_main'>
                 {mediaContent}
