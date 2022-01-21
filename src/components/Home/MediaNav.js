a911828afac5dba727b41290bfa40a27ef6faa11
robisonwebdev <david@robisonweb.dev>
2022-01-21 10:24:10 -0700

    Destructured items. Setup variable to map items, and return each item in an li. Gave nav a className of home_media_nav Called buildNav inside of nav.

--- src/components/Home/MediaNav.js
+++ src/components/Home/MediaNav.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const MediaNav = () => {
-  return (
-      <nav></nav>
-  );
+const MediaNav = ({ items }) => {
+    const buildNav = items.map(item => {
+        return <li key={item}>{item}</li>
+    });
+
+
+    return (
+        <nav className='home_media_nav'>
+            <ul>
+                {buildNav}
+            </ul>
+        </nav>
+    );
 };
 
 export default MediaNav;
