c08d13271626571a37bbbf146afa1fb9b0b72563
robisonwebdev <david@robisonweb.dev>
2022-01-21 10:58:39 -0700

    Imported Link and Card components. Destructured media prop. Setup buildCard variable to map over media and return Card component. Gave return div a className of home_media_content. Called buildCards inside of home_media_content.

--- src/components/Home/DisplayMedia.js
+++ src/components/Home/DisplayMedia.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from './Card';
 
-const DisplayMedia = () => {
+const DisplayMedia = ({ media }) => {
+    const buildCards = media.map(item => {
+        if (item.release_date) {
+            return <Link key={item.id} to={`movie/${item.id}`}><Card media={item} /></Link>
+        };
+        
+       return <Link key={item.id} to={`tv/${item.id}`}><Card media={item} /></Link>;
+    });
+
     return (
-        <div></div>
+        <div className='home_media_content'>
+            {buildCards}
+        </div>
     );
 };
 
