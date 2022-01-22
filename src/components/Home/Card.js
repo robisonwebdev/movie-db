183cce5f475516400133223786a6a6c3b6c998ab
robisonwebdev <david@robisonweb.dev>
2022-01-22 10:17:36 -0700

    Imported Link component. Removed poster variable. Setup function called getLinkPath to get the correct path for tv or movie. Setup getPoster function to get media poster and add Link to img. Changed return div to section with className of .home_media_card Removed img and replaced with getPoster. Changed cardContent to card_content Changed h4 to h2. Added Link component around h2.

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ShowDate from './ShowDate';
 import '../../styles/Home/Card.css';
 
 const Card = ({ media }) => {
     const airDate = media.first_air_date;
     const name = media.name
-    const poster = `https://image.tmdb.org/t/p/w185/${media.poster_path}`;
     const title = media.title;
     const releaseDate = media.release_date;
 
+    const getLinkPath = () => {
+        if (media.release_date) {
+            return `movie/${media.id}`;
+        };
+
+        return `tv/${media.id}`;
+    }
+
+    const getPoster = () => {
+        const noImage = 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';        
+
+        if (media.poster_path === null) {
+            return <Link to={getLinkPath()}><img src={noImage} alt={media.name || media.title} /></Link>
+        };
+
+        return <Link to={getLinkPath()}><img src={`https://image.tmdb.org/t/p/w220_and_h330_face${media.poster_path}`} alt={media.name || media.title} /></Link>
+    };
+
     return (
-        <div className='card'>
-            <img src={poster} alt={`${name || title} poster`} />
-            <div className='cardContent'>
-                <h4>{name || title}</h4>
+        <section className='home_media_card'>
+            {getPoster()}
+            <div className='card_content'>
+                <Link to={getLinkPath()}><h2>{name || title}</h2></Link>
                 <ShowDate value={airDate || releaseDate} />
-            </div>            
-        </div>
-    )
+            </div>
+        </section>
+    );
 };
 
 export default Card;
