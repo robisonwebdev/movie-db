7ef7d7e06585c477d28774c5bfb618b7779f392d
robisonwebdev <david@robisonweb.dev>
2022-01-23 12:56:19 -0700

    Setup fetchData function to fetch api data for trending day and week, and set it to there appropriate state.

--- src/components/Home/Trending/Trending.js
+++ src/components/Home/Trending/Trending.js
@@ -10,6 +10,29 @@ const Trending = () => {
     const [todayData, setTodayData] = useState([]);
     const [weekData, setWeekDate] = useState([]);
 
+    const fetchData = useCallback(() => {
+        const todayAPI = `https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`;
+        const weekAPI = `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}`;
+
+        const getTodayData = axios.get(todayAPI);
+        const getWeekData = axios.get(weekAPI);
+
+        setLoading(true);
+
+        axios
+        .all([getTodayData, getWeekData])
+        .then(axios.spread((...allData) => {
+            const today_data = allData[0].data.results;
+            const week_data = allData[1].data.results;
+
+            setTodayData(today_data);
+            setWeekDate(week_data);
+            setCardData(today_data);
+            setLoading(false);
+        }))
+        .catch(err => console.log(err))
+    }, []);
+
     return (
         <section className='home_media_container'>
 
