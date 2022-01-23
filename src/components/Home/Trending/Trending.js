c45e17331f0feecf8b452760d40080c31edaf095
robisonwebdev <david@robisonweb.dev>
2022-01-23 12:48:33 -0700

    Imported useCallback, useEffect, useState, axios, api_key, DisplayMedia, and MediaNAv. Gave return section a className of home_media_container

--- src/components/Home/Trending/Trending.js
+++ src/components/Home/Trending/Trending.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import api_key from '../../../data/Key';
+import DisplayMedia from '../DisplayMedia';
+import MediaNav from '../MediaNav';
 
 const Trending = () => {
     return (
-        <section></section>
+        <section className='home_media_container'>
+            
+        </section>
     );
 };
 
