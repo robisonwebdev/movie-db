1ee1a5cefa64a7b246c2a4555263184c5d9c8ac5
robisonwebdev <david@robisonweb.dev>
2022-01-24 10:37:18 -0700

    Fixed welcome import path.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FreeToWatch from './FreeToWatch/FreeToWatch';
 import Trending from './Trending/Trending';
-import Welcome from './Welcome';
+import Welcome from './Welcome/Welcome';
 import WhatsPopular from './WhatsPopular/WhatsPopular';
 import '../../styles/Home/Home.css';
 
