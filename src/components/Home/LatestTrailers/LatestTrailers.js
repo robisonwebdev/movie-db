3877e0ee910f48dc06ed6d4802c63fc68a0c00c5
robisonwebdev <david@robisonweb.dev>
2022-01-24 10:19:26 -0700

    Imported useCallback, useEffect, useState, axios, api_key, DisplayMedia, and MediaNav

--- src/components/Home/LatestTrailers/LatestTrailers.js
+++ src/components/Home/LatestTrailers/LatestTrailers.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import api_key from '../../../data/Key';
+import DisplayMedia from '../DisplayMedia';
+import MediaNav from '../MediaNav';
 
 const LatestTrailers = () => {
     return (
