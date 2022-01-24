be8d1b804a0bf16a23d8616c6e8a491aad016bc0
robisonwebdev <david@robisonweb.dev>
2022-01-24 10:38:53 -0700

    Fixed Search.css import path

--- src/components/Home/Welcome/Search.js
+++ src/components/Home/Welcome/Search.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
-import '../../styles/Home/Search.css';
+import '../../../styles/Home/Search.css';
 
 const Search = () => {
     const [value, setValue] = useState('');
