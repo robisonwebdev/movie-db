d4102d01f3db47056e7bdf3b8cf602e64bd0b675
robisonwebdev <david@robisonweb.dev>
2022-01-24 10:23:06 -0700

    Setup state for cardData, loading, rentData, theaterData, and tvData

--- src/components/Home/LatestTrailers/LatestTrailers.js
+++ src/components/Home/LatestTrailers/LatestTrailers.js
@@ -5,6 +5,12 @@ import DisplayMedia from '../DisplayMedia';
 import MediaNav from '../MediaNav';
 
 const LatestTrailers = () => {
+    const [cardData, setCardData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [rentData, setRentData] = useState([]);
+    const [theaterData, setTheaterData] = useState([]);
+    const [tvData, setTVData] = useState([]);
+
     return (
         <section className='home_media_container'>
             <MediaNav />
