d98ba6e94b9a42f8966687017c6539b1a8006efe
robisonwebdev <david@robisonweb.dev>
2022-01-28 13:47:15 -0700

    Changed className of return div. Changed return div to section.

--- src/components/Overview/Company/Company.js
+++ src/components/Overview/Company/Company.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Company = () => {
     return (
-        <div className='company_Overview'>
+        <section className='company_overview'>
             
-        </div>
+        </section>
     )
 };
 
