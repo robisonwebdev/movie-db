8f0e51e3f95c1b989b12d6a2496038511cad3630
robisonwebdev <david@robisonweb.dev>
2022-01-28 13:52:57 -0700

    Imported useCallback, useEffect, useState, useParams, axios, api_key, and Header component. Added Header component to return section.

--- src/components/Overview/Company/Company.js
+++ src/components/Overview/Company/Company.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import api_key from '../../../data/Key';
+import Header from './Header';
 
 const Company = () => {
     return (
         <section className='company_overview'>
-            
+            <Header />
         </section>
     )
 };
