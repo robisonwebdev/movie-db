a19f29bbe97c4033e8dee15e3d6ce2b74730292c
robisonwebdev <david@robisonweb.dev>
2022-01-29 10:35:38 -0700

    Destrucutred props. Setup getDetails function that return details of company in a list. Setup getLogo to get company logo, or name if logo null. Gave return section a className of overview_company_header. Inside section, added two divs with classNames of .company_header_upper and .company_header_lower Inside upper div, called getLogo. Inisde lower div, called getDetails.

--- src/components/Overview/Company/Header.js
+++ src/components/Overview/Company/Header.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({ data, format }) => {
+    const [companyData, companyMovieData, companyTVData] = data;
+
+    const getDetails = () => {
+      const name = companyData.name;
+      const headquarters = companyData.headquarters;
+      const country = companyData.origin_country;
+      const homepage = companyData.homepage;
+
+      return (
+        <ul>
+          {name === null ? null : <li><i class="las la-id-card-alt" /> {name}</li>}
+          {headquarters === null ? null : <li><i class="las la-map-marker-alt" /> {headquarters}</li>}
+          {country === null ? null : <li><i class="las la-globe-americas" /> {country}</li>}
+          {homepage === null ? null : <li><i class="las la-link" /> <a href={homepage} target='_blank'>Homepage</a></li>}
+        </ul>
+      );
+    }
+
+    const getLogo = () => {
+        if (companyData.logo_path === null) {
+            return companyData.name;
+        };
+
+        return <img src={`https://image.tmdb.org/t/p/h50_filter(negate,000,666)${companyData.logo_path}`} />
+    }
+
   return (
-    <section></section>
+    <section className='overview_company_header'>
+        {console.log('companyData', companyData)}
+        <div className='company_header_upper'>
+          {getLogo()}
+        </div>
+        <div className='company_header_lower'>
+          {getDetails()}
+        </div>
+    </section>
   );
 };
 
