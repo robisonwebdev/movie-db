7a7bd252beb2e53fc8d382ece13516205f53705f
robisonwebdev <david@robisonweb.dev>
2022-01-29 12:57:40 -0700

    Changed class to className on icons. In getLogo's if statement, wrap companyData.name in h2. Setup getMediaCount to get total results of movies or tv shows. Called getMediaCount in .company_header_upper

--- src/components/Overview/Company/Header.js
+++ src/components/Overview/Company/Header.js
@@ -12,28 +12,41 @@ const Header = ({ data, format }) => {
 
       return (
         <ul>
-          {name === null ? null : <li><i class="las la-id-card-alt" /> {name}</li>}
-          {headquarters === null ? null : <li><i class="las la-map-marker" /> {headquarters}</li>}
-          {country === null ? null : <li><i class="las la-globe-americas" /> {country}</li>}
-          {homepage === null ? null : <li><i class="las la-link" /> <a href={homepage} target='_blank'>Homepage</a></li>}
+          {name === null ? null : <li><i className="las la-id-card-alt" /> {name}</li>}
+          {headquarters === null ? null : <li><i className="las la-map-marker" /> {headquarters}</li>}
+          {country === null ? null : <li><i className="las la-globe-americas" /> {country}</li>}
+          {homepage === null ? null : <li><i className="las la-link" /> <a href={homepage} target='_blank'>Homepage</a></li>}
         </ul>
       );
     }
 
     const getLogo = () => {
         if (companyData.logo_path === null) {
-            return companyData.name;
+            return <h2>{companyData.name}</h2>;
         };
 
         return <img src={`https://image.tmdb.org/t/p/h50_filter(negate,000,666)${companyData.logo_path}`} />
     }
 
+    const getMediaCount = () => {
+      if (format === 'movie') {
+        return <h2>{companyMovieData.total_results} movies</h2>
+      }
+
+      if (format === 'shows') {
+        return <h2>{companyTVData.total_results} shows</h2>
+      }
+
+      return null;
+    }
+
   return (
     <section className='overview_company_header'>
       <div className='company_header_background'>
-        {console.log('companyData', companyData)}
+        {console.log('companyData', companyMovieData)}
         <div className='company_header_upper'>
           {getLogo()}
+          {getMediaCount()}
         </div>
         <div className='company_header_lower'>
           {getDetails()}
