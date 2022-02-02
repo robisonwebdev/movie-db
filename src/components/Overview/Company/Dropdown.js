9b4eef30ebc4a4298cdb8f5f0a065c2d6dd586f3
robisonwebdev <david@robisonweb.dev>
2022-02-02 16:26:19 -0700

    Imported useState. Setup state for show, with an initial value of false. Setup buildMenu variable to map over items. Gave return div a className of .overview_dropdown. Inside return div, added div with className of .overview_drodown_title, and ul inside a tenery. Setup onMouseEnter and onMouseLeave for .overview_dropdown_title. Calls setShow and passed true or false. Called buildMenu inside of ul.

--- src/components/Overview/Company/Dropdown.js
+++ src/components/Overview/Company/Dropdown.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dropdown = () => {
-    return <div></div>;
+const Dropdown = ({ items, title }) => {
+    const [show, setShow] = useState(false);
+
+    const buildMenu = items.map(item => {
+        return (<button className='overview_dropdown_item'>{item}</button>);
+    })
+
+    return (
+        <div className='overview_dropdown'>
+            <div className='overview_dropdown_title' onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+                {title} <i className='las la-sort-down' />
+            </div>
+            {show
+                ? <ul className='overview_dropdown_menu'>{buildMenu}</ul>
+                : null
+            }
+        </div>
+    );
 };
 
 export default Dropdown;
