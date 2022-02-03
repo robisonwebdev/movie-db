026c7f3292f17f27e1dc66109b800ff0f3e23f50
robisonwebdev <david@robisonweb.dev>
2022-02-02 17:26:06 -0700

    Imported Dropdown.js Changed path for Navigation.css Gave return section a className of .overview_navigation Inside sections, added ul with a className of .navigation_company Called Dropdown component in .navigation_company

--- src/components/Overview/Company/Navigation.js
+++ src/components/Overview/Company/Navigation.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Dropdown from './Dropdown';
+import '../../../styles/Overview/Navigation/Navigation.css';
 
 const Navigation = () => {
     return (
-        <section></section>
+        <section className='overview_navigation'>
+            <ul className='navigation_company'>
+                <Dropdown items={['Main', 'Changes']} title='Overview' />
+                <Dropdown items={[]} title='Movies' />
+                <Dropdown items={[]} title='Sort' />
+            </ul>
+        </section>
     );
 };
 
