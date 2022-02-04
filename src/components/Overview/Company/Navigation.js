eef2e4f74d7f6e0452496255844f0240e01ce1c5
robisonwebdev <david@robisonweb.dev>
2022-02-03 18:58:41 -0700

    Destrucutred handleNavChange prop. Pass handleNavChange to onClick prop for Dropdown components.

--- src/components/Overview/Company/Navigation.js
+++ src/components/Overview/Company/Navigation.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Dropdown from './Dropdown';
 import '../../../styles/Overview/Navigation/Navigation.css';
 
-const Navigation = () => {
+const Navigation = ({ handleNavChange }) => {
     return (
         <section className='overview_navigation'>
             <ul className='navigation_company'>
-                <Dropdown items={['Main', 'Changes']} title='Overview' />
-                <Dropdown items={['Movies', 'TV Shows']} title='Movies' />
-                <Dropdown items={['Popularity', 'Rating', 'Release Date']} title='Sort' />
+                <Dropdown items={['Main', 'Changes']} onClick={handleNavChange} title='Overview' />
+                <Dropdown items={['Movies', 'TV Shows']} onClick={handleNavChange} title='Movies' />
+                <Dropdown items={['Popularity', 'Rating', 'Release Date']} onClick={handleNavChange} title='Sort' />
             </ul>
         </section>
     );
